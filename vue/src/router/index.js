0837862cbc652f29d3475945fff400b79313de11
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-02 16:08:54 +0200

    update: index.js Paths

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
+
+import DefaultLayout from '../Layouts/DefaultLayout.vue'
+
 import Start from '../views/Start.vue'
 import Portfolio from '../views/Portfolio.vue'
 import Oferta from '../views/Oferta.vue'
@@ -7,8 +10,13 @@ import Kontakt from '../views/Kontakt.vue'
 const routes = [
     {
         path: '/',
-        name: 'Start',
-        component: Start
+        // redirect: '/start',
+        // name: 'Start',
+        component: DefaultLayout,
+        children: [
+            { path: '/', name: 'StartFirst', component: Start },
+            { path: '/start', name: 'Start', component: Start }
+        ]
     },
     {
         path: '/portfolio',
