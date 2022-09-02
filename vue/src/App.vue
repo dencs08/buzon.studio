c5c4c2b3c9441bfe752aadced22dc69c0acff51f
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-02 16:09:03 +0200

    update: App.vue navbar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,24 +1,18 @@
-<script setup>
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import navbar from "./components/Navbar/Navbar.vue";
+export default {
+    components: {
+        navbar, // register component
+    },
+};
 import "./css/app.css";
 </script>
 
 <template>
+    <navbar />
     <router-view></router-view>
 </template>
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
