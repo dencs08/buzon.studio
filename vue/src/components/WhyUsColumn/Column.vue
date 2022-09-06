475be29a61f0147e83faceecb38c46fc48081cd2
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-06 15:21:44 +0200

    add: AboutUs container & Column component

---
+++ vue/src/components/WhyUsColumn/Column.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="px-0 md:px-3 lg:px-5 mb-4 md:mb-0 link-activate-wrapper">
+        <a href="/oferta">
+            <div class="offer-wrapper">
+                <div class="flex justify-center">
+                    <img src="../../assets/images/branding2.jpg" alt="" />
+                </div>
+
+                <div class="absolute top-[94%] my-0 ml-2 md:ml-0">
+                    <a href="/oferta" class="font-family-header my-0">
+                        <h3 class="my-0 link-bracket-display link-move">
+                            Produkty cyfrowe
+                        </h3>
+                    </a>
+                </div>
+            </div>
+            <div class="mt-3 mb:mt-6 lg:mt-10 pr-3 md:pr-0 ml-2 md:ml-0">
+                <p class="font-color-dark">
+                    Tworzymy i rozwijamy, strony internetowe, aplikacje,
+                    wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje
+                    potrzeby, tak abyś otrzymał najlepiej dostosowany produkt
+                    który pracuje dla Ciebie.
+                </p>
+                <ul class="uppercase font-color-darker mt-2">
+                    <li>
+                        <span>Strony internetowe</span>
+                    </li>
+                    <li>
+                        <span>E-commerce</span>
+                    </li>
+                    <li>
+                        <span>UI / UX </span>
+                    </li>
+                    <li>
+                        <span>Projekty graficzne</span>
+                    </li>
+                    <li>
+                        <span>Wizualizacje 3D</span>
+                    </li>
+                    <li>
+                        <span>Modelowanie 3D</span>
+                    </li>
+                    <li>
+                        <span>Aplikacje</span>
+                    </li>
+                </ul>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script></script>
+
+<style>
+@import "./column.scss";
+</style>
---
+++ vue/src/containers/AboutUs/AboutUs.vue
@@ -0,0 +1,31 @@
+<template>
+    <section id="aboutUs">
+        <div class="container grid grid-cols-8">
+            <div class=""></div>
+            <div class="col-start-6 col-end-9">
+                <p class="font-color-dark">
+                    <b class="uppercase">{{ name }}</b> to 5 lat doświadczenia w
+                    tworzeniu projektów i pomaganiu markom w zwiększaniu
+                    efektywności ich działania. Stawiamy czoło wyzwaniom i
+                    osiągamy cele poprzez współpracę z Wami. Dzięki naszej
+                    pracy, wspólnie możemy uzyskać wspaniałe efekty.
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+let name = import.meta.env.VITE_NAME;
+export default {
+    data() {
+        return {
+            name: name,
+        };
+    },
+};
+</script>
+
+<style>
+@import "./aboutus.scss";
+</style>
