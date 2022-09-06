3216732279aef999a068229f28c034cc47bb922d
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-06 16:29:19 +0200

    update: WhyUs container now uses Column component

--- vue/src/components/WhyUsColumn/Column.vue
+++ vue/src/components/WhyUsColumn/Column.vue
@@ -9,39 +9,18 @@
                 <div class="absolute top-[94%] my-0 ml-2 md:ml-0">
                     <a href="/oferta" class="font-family-header my-0">
                         <h3 class="my-0 link-bracket-display link-move">
-                            Produkty cyfrowe
+                            {{ title }}
                         </h3>
                     </a>
                 </div>
             </div>
             <div class="mt-3 mb:mt-6 lg:mt-10 pr-3 md:pr-0 ml-2 md:ml-0">
                 <p class="font-color-dark">
-                    Tworzymy i rozwijamy, strony internetowe, aplikacje,
-                    wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje
-                    potrzeby, tak abyś otrzymał najlepiej dostosowany produkt
-                    który pracuje dla Ciebie.
+                    {{ paragraph }}
                 </p>
                 <ul class="uppercase font-color-darker mt-2">
-                    <li>
-                        <span>Strony internetowe</span>
-                    </li>
-                    <li>
-                        <span>E-commerce</span>
-                    </li>
-                    <li>
-                        <span>UI / UX </span>
-                    </li>
-                    <li>
-                        <span>Projekty graficzne</span>
-                    </li>
-                    <li>
-                        <span>Wizualizacje 3D</span>
-                    </li>
-                    <li>
-                        <span>Modelowanie 3D</span>
-                    </li>
-                    <li>
-                        <span>Aplikacje</span>
+                    <li v-for="item in ul">
+                        {{ item }}
                     </li>
                 </ul>
             </div>
@@ -49,7 +28,15 @@
     </div>
 </template>
 
-<script></script>
+<script>
+export default {
+    props: {
+        title: String,
+        paragraph: String,
+        ul: Array,
+    },
+};
+</script>
 
 <style>
 @import "./column.scss";
---
+++ vue/src/containers/WhyUs/WhyUs.vue
@@ -0,0 +1,61 @@
+<template>
+    <section id="offer" class="section-mb">
+        <div class="container mb-2 lg:mb-4 xl:mb-5">
+            <div>
+                <h3 class="headerline">Działamy po to abyś się rozwijał</h3>
+            </div>
+            <div class="grid grid-cols-2">
+                <Column
+                    title="Produkty cyfrowe"
+                    paragraph="Tworzymy i rozwijamy, strony internetowe, aplikacje, wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje potrzeby, tak abyś otrzymał najlepiej dostosowany produkt który pracuje dla Ciebie."
+                    :ul="[
+                        'Strony internetowe',
+                        'E-commerce',
+                        'UI / UX',
+                        'Projekty graficzne',
+                        'Wizualizacje 3D',
+                        'Modelowanie 3D',
+                        'Aplikacje',
+                    ]"
+                />
+                <Column
+                    title="Branding"
+                    paragraph="Dziś branding jest najważniejszym elementem każdego dobrze prosperującego biznesu, łatwe do zapamiętania logo, charakterystyczna identyfikacja wizualna czy dobrze obrana strategia marketingowa, ma ogromne znaczenie w jaki sposób będziesz odbierany na rynku"
+                    :ul="[
+                        'Nazewnictwo',
+                        'Identyfikacja wizualna',
+                        'Logotypy',
+                        'Marketing',
+                        'Strategie biznesowe w social mediach',
+                    ]"
+                />
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="link-activate-wrapper contact">
+                <a href="/kontakt" class="underline-primary font-color-primary"
+                    >Napisz do nas
+                </a>
+                <a href="/kontakt" class="font-color-dark">
+                    i stwórzmy coś razem
+                </a>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import { Column } from "../../components";
+export default {
+    components: {
+        Column,
+    },
+};
+
+console.log(window.APP_NAME);
+</script>
+
+<style>
+@import "./whyus.scss";
+</style>
