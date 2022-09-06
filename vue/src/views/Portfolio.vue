9e6c85d7a2a5ab56901a1b06ab89f7e471760986
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-07 00:46:48 +0200

    update: Portfolio view content

--- vue/src/views/Portfolio.vue
+++ vue/src/views/Portfolio.vue
@@ -1,7 +1,32 @@
 <template>
-    <h1>Portfolio</h1>
+    <div id="fullpage">
+        <div
+            section
+            class="grid container grid-cols-1 md:grid-cols-2 h-screen place-items-center"
+        >
+            <PortfolioFullpage
+                title="bielanypark"
+                info="info"
+                :category="['test1', 'test2']"
+            />
+            <PortfolioFullpage
+                title="danfit"
+                info="info"
+                :category="['test1', 'test2']"
+            />
+        </div>
+        <div>
+            <div section>
+                <div>
+                    <Footer />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default { name: "Portfolio" };
+import { Footer } from "../containers";
+import { PortfolioFullpage } from "../components";
+export default { name: "Portfolio", components: { Footer, PortfolioFullpage } };
 </script>
