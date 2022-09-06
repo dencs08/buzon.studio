245e61c8fbef86aae85a871c7f1215c6383f58a9
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-06 11:11:26 +0200

    update: App.vue styles update

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,13 +1,11 @@
 <script>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import navbar from "./components/Navbar/Navbar.vue";
 export default {
     components: {
-        navbar, // register component
+        navbar,
     },
 };
-import "./css/app.css";
+import "./styles/app.scss";
 </script>
 
 <template>
