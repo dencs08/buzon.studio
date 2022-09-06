895567c842b183d942def1d1662e03483d7a3683
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-06 11:09:48 +0200

    add: index.js for component & container folders

---
+++ vue/src/containers/index.js
@@ -0,0 +1,5 @@
+export { default as heroSection } from './Hero/Hero.vue'
+export { default as ourWorksSection } from './OurWorks/OurWorks.vue'
+export { default as footerSection } from './Footer/Footer.vue'
+
+
