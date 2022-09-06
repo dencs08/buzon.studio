fa5e35f09a60052814b1d9417a6b18cd22262437
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-06 11:08:22 +0200

    update: Hero section content

--- vue/src/containers/Hero/Hero.vue
+++ vue/src/containers/Hero/Hero.vue
@@ -1,4 +1,34 @@
-<template><div class="h-screen w-screen">Hero</div></template>
+<template>
+    <section id="landing-page" class="flex items-center">
+        <div class="container">
+            <h1>BUZONSTUDIO INNOWACYJNE USŁUGI IT.</h1>
+
+            <h2 class="fw-normal font-family-primary pe-0 pe-md-6">
+                Jesteśmy kreatywnym studio w branży IT zajmującym sie
+                dostarczniem najlepszych
+                <a
+                    href="/portfolio"
+                    class="link-primary underline-primary fw-bold"
+                    >produktów</a
+                >
+                i
+                <a href="/oferta" class="link-primary underline-primary fw-bold"
+                    >usług</a
+                >
+                IT. Tworzymy strony internetowe, wizualizacje, branding,
+                marketing online i inne produkty cyfrowe z pomocą metody
+                <span
+                    data-tooltip="Dzięki All-in-One zaosczędzisz czas i pieniądze, nie martw się o zatrudnianie kilku zespołów osobno - my realizujemy wszystko od A do Z."
+                    class="text-highlight cursor-pointer"
+                    ><a href="/oferta" class="link-primary underline-primary"
+                        >All-in-One.</a
+                    ></span
+                >
+            </h2>
+            <!-- <x-arrow/> -->
+        </div>
+    </section>
+</template>
 
 <script></script>
 
