e30928881954f1ccf00e17c18443fe2124a89a6e
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-07 00:44:58 +0200

    add: Projekt & Prywatnosc view

---
+++ vue/src/views/Projekt.vue
@@ -0,0 +1,21 @@
+<template>
+    <ProjectHero :name="$route.params.name" :image="image" />
+    <ProjectInfo />
+    <ProjectImages />
+    <Footer />
+</template>
+
+<script>
+import { Footer, ProjectHero, ProjectInfo, ProjectImages } from "../containers";
+export default {
+    name: "Projekt",
+    components: { Footer, ProjectHero, ProjectInfo, ProjectImages },
+    computed: {
+        image() {
+            return `../src/assets/images/portfolio/${this.$route.params.name}/${this.$route.params.name}_portfolio.jpg`;
+        },
+    },
+};
+</script>
+
+<style></style>
