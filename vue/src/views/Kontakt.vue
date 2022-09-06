02945934d040887f10d220d8c128672545b513a0
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-07 00:47:09 +0200

    update: Kontakt view content

--- vue/src/views/Kontakt.vue
+++ vue/src/views/Kontakt.vue
@@ -1,7 +1,20 @@
 <template>
-    <h1>Kontakt</h1>
+    <section id="landing-page">
+        <div class="container pt-32">
+            <h1 class="leading-[100%]">Zacznijmy owocną współpracę!</h1>
+
+            <h2 class="mb-5">
+                Powiedz nam czego potrzebujesz, a my zrealizujemy to od A do Z.
+            </h2>
+
+            <!-- <x-arrow /> -->
+        </div>
+    </section>
+    <EmailForm />
+    <Footer />
 </template>
 
 <script>
-export default { name: "Kontakt" };
+import { Footer, EmailForm } from "../containers";
+export default { name: "Kontakt", components: { Footer, EmailForm } };
 </script>
