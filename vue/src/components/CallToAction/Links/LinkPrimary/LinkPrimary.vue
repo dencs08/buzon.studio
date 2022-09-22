4a1e99e1351db02f7b7282895d26d262a2c21f0e
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-22 22:24:12 +0200

    update: LinkPrimary component functionality

--- vue/src/components/CallToAction/Links/LinkPrimary/LinkPrimary.vue
+++ vue/src/components/CallToAction/Links/LinkPrimary/LinkPrimary.vue
@@ -1,7 +1,47 @@
-<template></template>
+<template>
+    <a
+        :href="href"
+        :class="{
+            'underline-primary': primary,
+            'underline-secondary': secondary,
+            animating: isAnimating,
+        }"
+        @mouseover="animationStart"
+        @animationend="animationEnd"
+        >{{ text }}</a
+    >
+</template>
 
-<script></script>
+<script>
+export default {
+    data() {
+        return {
+            isAnimating: false,
+        };
+    },
+    props: {
+        href: String,
+        text: String,
 
-<style scoped>
+        primary: {
+            type: Boolean,
+        },
+        secondary: {
+            type: Boolean,
+        },
+    },
+    methods: {
+        animationStart() {
+            this.isAnimating = true;
+        },
+        animationEnd() {
+            this.isAnimating = false;
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+// [secondary ? 'underline-secondary' : 'underline-primary']
 @import "./linkprimary.scss";
 </style>
