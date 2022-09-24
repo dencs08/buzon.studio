625c2a88e512b87f3e6d6cc40386f1aafc813aec
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-24 21:55:15 +0200

    update: Components index.js exports

--- vue/src/components/index.js
+++ vue/src/components/index.js
@@ -1,3 +1,10 @@
+export { default as FB } from './SVG/FB/FB.vue'
+export { default as IG } from './SVG/IG/IG.vue'
+export { default as Logo } from './SVG/Logo/Logo.vue'
+
+export { default as ItemHeader } from './Headers/ItemHeader/ItemHeader.vue'
+export { default as SectionHeader } from './Headers/SectionHeader/SectionHeader.vue'
+
 export { default as SliderItem } from './PortfolioSliderItem/SliderItem.vue'
 export { default as Column } from './WhyUsColumn/Column.vue'
 export { default as CollabItem } from './CollabItem/CollabItem.vue'
