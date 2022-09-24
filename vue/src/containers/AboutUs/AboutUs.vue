3c2cae6969fcff884a1dbe10feab5e778e18f320
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-24 21:58:49 +0200

    update: AboutUs container {{name}} bold

--- vue/src/containers/AboutUs/AboutUs.vue
+++ vue/src/containers/AboutUs/AboutUs.vue
@@ -4,11 +4,11 @@
             <div class=""></div>
             <div class="col-start-6 col-end-9">
                 <p class="font-color-dark">
-                    <b class="uppercase">{{ name }}</b> to 5 lat doświadczenia w
-                    tworzeniu projektów i pomaganiu markom w zwiększaniu
-                    efektywności ich działania. Stawiamy czoło wyzwaniom i
-                    osiągamy cele poprzez współpracę z Wami. Dzięki naszej
-                    pracy, wspólnie możemy uzyskać wspaniałe efekty.
+                    <b class="uppercase font-bold">{{ name }}</b> to 5 lat
+                    doświadczenia w tworzeniu projektów i pomaganiu markom w
+                    zwiększaniu efektywności ich działania. Stawiamy czoło
+                    wyzwaniom i osiągamy cele poprzez współpracę z Wami. Dzięki
+                    wspólnej pracy, wspólnie możemy uzyskać wspaniałe efekty.
                 </p>
             </div>
         </div>
