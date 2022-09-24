1ef2160aef25f6d728b82cb8715e94e49a5abe79
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-24 21:55:02 +0200

    add: SectionHeader.vue

---
+++ vue/src/components/Headers/SectionHeader/SectionHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h4 :class="{ headerline: noAccent === false }">{{ headerLine }}</h4>
+        <h3 :class="{ big: big, break: wordBreak }">{{ mainHeader }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        headerLine: String,
+        mainHeader: String,
+        noAccent: {
+            type: Boolean,
+            default: () => false,
+        },
+
+        big: {
+            type: Boolean,
+            default: () => false,
+        },
+
+        wordBreak: {
+            type: Boolean,
+            default: () => false,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./sectionheader.scss";
+</style>
