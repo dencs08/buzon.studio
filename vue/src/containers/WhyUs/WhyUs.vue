60fdd5da82118658217b29d96be0ea38fe74deed
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-27 14:25:12 +0200

    update: WhyUs.vue use updated column component

--- vue/src/containers/WhyUs/WhyUs.vue
+++ vue/src/containers/WhyUs/WhyUs.vue
@@ -2,37 +2,41 @@
     <section id="offer" class="section-mb">
         <div class="container mb-8 lg:mb-12 xl:mb-20">
             <div class="mb-8 md:mb-12">
-                <SectionHeader mainHeader="Działamy po to abyś się rozwijał" />
-            </div>
-            <div class="grid grid-cols-2">
-                <Column
-                    title="Produkty cyfrowe"
-                    paragraph="Tworzymy i rozwijamy, strony internetowe, aplikacje, wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje potrzeby, tak abyś otrzymał najlepiej dostosowany produkt który pracuje dla Ciebie."
-                    :ul="[
-                        'Strony internetowe',
-                        'E-commerce',
-                        'UI / UX',
-                        'Projekty graficzne',
-                        'Wizualizacje 3D',
-                        'Modelowanie 3D',
-                        'Aplikacje',
-                    ]"
-                />
-                <Column
-                    title="Branding"
-                    paragraph="Dziś branding jest najważniejszym elementem każdego dobrze prosperującego biznesu, łatwe do zapamiętania logo, charakterystyczna identyfikacja wizualna czy dobrze obrana strategia marketingowa, ma ogromne znaczenie w jaki sposób będziesz odbierany na rynku"
-                    :ul="[
-                        'Nazewnictwo',
-                        'Identyfikacja wizualna',
-                        'Logotypy',
-                        'Marketing',
-                        'Strategie biznesowe w social mediach',
-                    ]"
+                <SectionHeader
+                    v-if="header"
+                    mainHeader="Działamy po to abyś się rozwijał"
                 />
             </div>
+            <Column
+                title="Produkty cyfrowe"
+                paragraph="Tworzymy i rozwijamy, strony internetowe, aplikacje, wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje potrzeby, tak abyś otrzymał najlepiej dostosowany produkt który pracuje dla Ciebie."
+                :ul="[
+                    'Strony internetowe',
+                    'E-commerce',
+                    'UI / UX',
+                    'Projekty graficzne',
+                    'Wizualizacje 3D',
+                    'Modelowanie 3D',
+                    'Aplikacje',
+                ]"
+                :buttons="buttons"
+            />
+            <Column
+                title="Branding"
+                paragraph="Dziś branding jest najważniejszym elementem każdego dobrze prosperującego biznesu, łatwe do zapamiętania logo, charakterystyczna identyfikacja wizualna czy dobrze obrana strategia marketingowa, ma ogromne znaczenie w jaki sposób będziesz odbierany na rynku"
+                :ul="[
+                    'Nazewnictwo',
+                    'Identyfikacja wizualna',
+                    'Logotypy',
+                    'Marketing',
+                    'Strategie biznesowe w social mediach',
+                ]"
+                :reverse="true"
+                :buttons="buttons"
+            />
         </div>
 
-        <div class="container">
+        <div v-if="contactParagraph" class="container">
             <a href="/kontakt" class="link-activate-wrapper-paragraph contact">
                 <LinkPrimary
                     href="/kontakt"
@@ -49,6 +53,19 @@
 <script>
 import { Column, LinkPrimary, SectionHeader } from "../../components";
 export default {
+    props: {
+        header: {
+            type: Boolean,
+            default: () => false,
+        },
+        contactParagraph: {
+            type: Boolean,
+            default: () => false,
+        },
+        buttons: {
+            type: Boolean,
+        },
+    },
     components: {
         Column,
         LinkPrimary,
