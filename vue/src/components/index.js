68abf82b842b3d0f69b12ce4800a85332fea7a4c
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-27 14:22:41 +0200

    add: components for svgs

--- vue/src/components/index.js
+++ vue/src/components/index.js
@@ -1,6 +1,10 @@
 export { default as FB } from './SVG/FB/FB.vue'
 export { default as IG } from './SVG/IG/IG.vue'
 export { default as Logo } from './SVG/Logo/Logo.vue'
+export { default as LogoWhite } from './SVG/LogoWhite/LogoWhite.vue'
+
+export { default as LinkPrimary } from './CallToAction/Links/LinkPrimary/LinkPrimary.vue'
+export { default as PrimaryButton } from './CallToAction/Buttons/PrimaryButton/PrimaryButton.vue'
 
 export { default as ItemHeader } from './Headers/ItemHeader/ItemHeader.vue'
 export { default as SectionHeader } from './Headers/SectionHeader/SectionHeader.vue'
@@ -10,5 +14,3 @@ export { default as Column } from './WhyUsColumn/Column.vue'
 export { default as CollabItem } from './CollabItem/CollabItem.vue'
 export { default as OurProcessItem } from './OurProcessItem/OurProcessItem.vue'
 export { default as PortfolioFullpage } from './PortfolioFullpage/PortfolioFullpage.vue'
-
-export { default as LinkPrimary } from './CallToAction/Links/LinkPrimary/LinkPrimary.vue'
