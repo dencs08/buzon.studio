6aa3467d2644504cdebe7431e8ab1de7ba3331c6
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-09-30 20:40:21 +0200

    update: OurProcessItem.vue activates ItemHeader

--- vue/src/components/OurProcessItem/OurProcessItem.vue
+++ vue/src/components/OurProcessItem/OurProcessItem.vue
@@ -1,10 +1,15 @@
 <template>
-    <div class="link-activate-wrapper">
+    <div
+        class="link-activate-wrapper"
+        @mouseover="mouseOver"
+        @mouseleave="mouseLeave"
+    >
         <ItemHeader
             :text="title"
             :small="true"
             :move="true"
             headerClass="font-color-secondary mb-2"
+            ref="itemHeader"
         />
 
         <p class="font-color-dark pr-0 md:pr-5">
@@ -17,12 +22,29 @@
 import { ItemHeader } from "../";
 
 export default {
+    data() {
+        return {
+            isMouseOver: false,
+        };
+    },
+
     props: {
         title: String,
         paragraph: String,
     },
     components: {
         ItemHeader,
+    },
+
+    methods: {
+        mouseOver() {
+            this.$refs.itemHeader.mouseOver();
+            this.isMouseOver = true;
+        },
+        mouseLeave() {
+            this.$refs.itemHeader.mouseLeave();
+            this.isMouseOver = false;
+        },
     },
 };
 </script>
