4dcff4f733fd05910034e16ec26eb44fae6e08a4
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-02 13:42:30 +0200

    add: ButtonTrail.vue

---
+++ vue/src/components/CallToAction/Buttons/ButtonTrail/ButtonTrail.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="button-trail">
+        <form>
+            <a href="/kontakt">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                {{ text }}
+            </a>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        text: String,
+    },
+};
+</script>
+<style lang="scss" scoped>
+@import "./buttontrail.scss";
+</style>
--- vue/src/components/CallToAction/Buttons/TrailButton/TrailButton.vue
+++
@@ -1,7 +0,0 @@
-<template></template>
-
-<script></script>
-
-<style scoped>
-@import "./trailbutton.scss";
-</style>
