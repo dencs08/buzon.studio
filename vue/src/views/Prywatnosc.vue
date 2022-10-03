ec05c9abfc3cd053e497e4149d0cd4122cbac2d0
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-03 16:56:50 +0200

    update: Prywatnosc.vue typos & variables

--- vue/src/views/Prywatnosc.vue
+++ vue/src/views/Prywatnosc.vue
@@ -1,120 +1,153 @@
 <template>
-    <div class="container pt-32">
-        <h1>Polityka prywatności {{ name }}</h1>
+    <div>
+        <div class="container pt-32">
+            <h1>Polityka prywatności {{ name }}</h1>
+        </div>
+
+        <div class="container space-y-8">
+            <p>
+                Ta polityka prywatności (“<strong>Polityka</strong>”) zawarta na
+                tej Stronie zarządza prywatnością informacji naszych
+                użytkowników i odwiedzających, które zbieramy, używamy,
+                ujawniamy i zarządzamy poprzez używanie Strony
+                {{ name }} (“<strong>Strony</strong>”).<br />
+                Poprzez używanie Strony, zgadzasz się w pełni na warunki
+                polityki zawartej w niniejszym dokumencie i oświadczasz, że
+                Twoja odpowiedzialnością jest zapoznanie się z nią i z
+                wprowadzonymi do niej poprawkami. Jeśli nie akceptujesz tej
+                polityki prywatności, proszę nie korzystaj ze Strony.
+            </p>
+            <p>
+                <strong>1. Uzywanie informacji</strong><br />
+                
+                Poprzez wejście na stronę oświadczasz, że możemy użyć Twoich
+                informacji do którychkolwiek z podanych poniżej powodów,
+                kiedykolwiek będzie to potrzebne bez żadnych ograniczeń:
+                Rejestrowanie, zarządzanie Twoim kontem, oraz tworzenie
+                spersonalizowanego profilu do kolejnych odwiedzin na Stronie.
+                <br />
+                <ul class="dots">
+                    <li>Zarządzanie spotkaniami i ich rezerwowanie.</li>
+                    <li>Powiadamiać Cię o nowościach.</li>
+                    <li>Używać w sprawie komunikacji user-to-user.</li>
+                    <li>
+                        Używać do jakiejkolwiek aktywności związanej z projektami{{
+                            name
+                        }}.
+                    </li>
+                    <li>
+                        Tworzenie kontentu, reklam, akcji promocyjnych i ofert
+                        rekomendowanych dla Ciebie.
+                    </li>
+                    <li>
+                        Prosić o opinię i monitorować użytkowanie Strony w celu
+                        usprawnienia Twojego doświadczenia.
+                    </li>
+                    <li>Odpowadać na zapytania klientów.</li>
+                </ul>
+            </p>
+            <p>
+                <strong>2. Zbieranie informacji</strong><br />
+                Możemy zbierać informacje o Tobie między innymi takich jak np.:
+                Dane personalne, dane pochodne, dane z social mediów lub dane
+                sieciowe, dane finansowe, włącznie i bez żadnych limitów: Twoje
+                imię, narodowość, data urodzenia, miejsce zamieszkania,
+                informacje kontaktowe, wiek, płeć, lub jakichkolwiek innych
+                informacji które nasza Strona zbiera automatycznie kiedy
+                wejdziesz na stronę. (np. rodzaj przeglądarki, system
+                operacyjny, czas użytkowania, Strony ktore odwiedziles przed
+                odwiedzeniem naszej Strony.)
+            </p>
+            <p>
+                <strong>3. Ujawnianie informacji</strong><br />
+                3.1 Oświadczasz, że możemy udostępniać Twoje informacji z
+                naszymi partnerami biznesowymi, inwestorami, reklamodawcami,
+                dostawcami usług marketingowych czy osobami współpracującymi, w
+                celach usprawnienia Strony i doświadczenia które dostarczamy
+                klientom online. 3.2 Oświadczasz, że możemy współpracować z
+                zewnętrznymi dostawcami / sprzedawcami lub zewnętrznym
+                oprogramowaniem aby reklamować lub tworzyć i używać kampanii
+                marketingowych na tej Stronie i poza nią. Jednocześnie
+                oświadczając zgodę na to, Ci zewnętrzni dostawcy również mogą
+                zbierać Twoje dane i je wykorzystywać w celu usprawnienia tej
+                Strony. A tym samym jesteś odpowiedzialny za zapoznanie się z
+                ich polityką prywatności i skontaktowanie się z nimi jeśli masz
+                jakiekolwiek wątpliwości.
+            </p>
+            <p>
+                <strong>4. Bezpieczeństwo informacji</strong><br />
+                Użyjemy rozsądnych środków i kroków aby zabezpieczyć i zarządzać
+                Twoimi danymi które zebraliśmy i otrzymaliśmy od Ciebie. Pomimo
+                naszych starań nie mozemy zagwarantowac bezpieczeństwa i
+                prywatności Twoich danych, żadne środki bezpieczeństwa nie są
+                nieprzenikalny przed jakimkolwiek przechwyceniem lub innym
+                rodzajem niewłaściwego użytkowania.
+            </p>
+            <p>
+                <strong>5. Polityka ciasteczek</strong><br />
+                Możemy używać ciasteczek, web beacon’ów i innych technologii
+                śledzących na tej Stronie, które domyślnie są ustawione na
+                akceptację ich ciasteczek, w celu polepszenia doświadczenia
+                online dla naszych klientów. Musisz być świadomym tego, że
+                zablokowanie ciasteczek lub web beacon’ów może wpłynąć na
+                funkcjonalność tej Strony.
+            </p>
+            <p>
+                <strong>6. Treść stron trzecich</strong><br />
+                Jakakolwiek informacje, które dostarczysz stronom trzecim - nie
+                związanych z nami - nie są pokryte w tej Polityce. Nie jesteśmy
+                odpowiedzialni za zawartość, prywatność i bezpieczeństwo
+                polityki jakiejkolwiek ze stron trzecich, włączając w to inne
+                Strony internetowe, reklamy, zewnętrzne seerwisy lub aplikacje,
+                które mogą być powiązane do tej Strony.
+            </p>
+            <p>
+                <strong>7. Ograniczenie odpowiedzialności</strong><br />
+
+                Nie jesteśmy odpowiedzialni za działalność stron trzecich z,
+                którym udostępniłeś lub wymieniłeś swoje dane w jakiejkolwiek
+                formie i nie mamy możliwości zarządzania lub kontrolowania ich.
+            </p>
+            <p>
+                <strong>8. Komunikacja</strong><br />
+                Jeśli nie życzysz sobie otrzymywania jakichkolwiek informacji od
+                nas, możesz zrezygnować poprzez skontaktowanie się z nami
+                poprzez email lub jakąkolwiek inną formę kontaktu która jest
+                zawarta na tej Stronie.
+            </p>
+            <p>
+                <strong>9. Modyfikacja polityki prywatności</strong><br />
+                Przyjmujesz do wiadomości, że ta polityka prywatności jest
+                obiektem modyfikacji, alternacji i poprawek przez
+                {{ name }} przez cały czas, bez lub z powiadomieniem Ciebie o
+                nich. Zgadzasz się również z tym, że jakiekolwiek modyfikacje są
+                w ważne i mają wpływ po ich zamieszczeniu w tej Witrynie.
+            </p>
+        </div>
+        <Footer />
     </div>
-
-    <div class="container space-y-8">
-        <p>
-            Ta polityka prywatności (“<strong>Polityka</strong>”) zawarta na tej
-            Stronie zarządza prywatnością informacji naszych uzytkownikow i
-            odwiedzających, które zbieramy, uzywmy, ujawniamy i zarządzamy
-            poprzez uzywanie Strony
-            {{ name }} (“<strong>Strony</strong>”).<br />
-            Poprzez uzywanie Strony, zgadzasz sie w pełni na warunki polityki
-            zawartej w ninejszym dokumencie i oświadzasz, ze Twoja
-            odpowiedzialnością jest zapoznanies się z nią i z prowadzanymi do
-            niej poprawkami. Jeśli nie akceptujesz takiej polityki prywatności,
-            proszę nie korzystaj ze Strony.
-        </p>
-        <p>
-            <strong>1. Uzywanie informacji</strong><br />
-
-            Poprzez wejście na stronę oświadzasz, ze mozemy uzyc Twoich
-            informacji do ktorychkolwiek z podanych ponizej powodów,
-            kiedykolwiek będzie to potrzebne bez zadnych ograniczeń: <br />
-            Rejestrowanie, zarządzanie Twoim kontem, oraz tworzenie
-            spersonalizowanego profilu do kolejnych odwiedzin na Stronie. <br />
-            Zarządzanie spotkaniami i ich rezerwowanie. <br />
-            Powiadamiać Cię o nowościach. <br />
-            Uzywac w sprawie komunikacji user-to-user.<br />
-            Uzywac do jakiejkolwiek aktywnosci ziwązanej z projektami
-            {{ name }}. <br />
-            Tworzenie kontentu, reklam, akcji promocyjnych i ofert
-            rekomendownych dla Ciebie.<br />
-            Prosić o opinię i monitorować uzytkowanie Strony w celu usprawnienia
-            Twojego doświadczenia.<br />
-            Odpowadać na zapytania klientów.
-        </p>
-        <p>
-            <strong>2. Zbieranie informacji</strong><br />
-            Mozemy zbierac informacje o Tobie między innymi takich jak np.: Dane
-            personalne, dane pochodne, dane z social mediów lub dane sieciowe,
-            dane finansowe, włącznie i bez zadnych limitów: Twoje imie,
-            narodowość, data urodzenia, miejsce zamieszkania, informacje
-            kontaktowe, wiek, płeć, lub jakichkolwiek innych informacji które
-            nasza Strona zbiera automatycznie kiedy wejdziesz na stronę. (np.
-            rodzaj przeglądarki, system operacyjny, czas uzytkowania, Strony
-            ktore odwiedziles przed odwiedzeniem naszej Strony.)
-        </p>
-        <p>
-            <strong>3. Ujawnianie informacji</strong><br />
-            3.1 Oświadzasz, ze mozemy udostepniac Twoje informacji z naszymi
-            partnerami bieznesowymi, inwestorami, reklamodawcami, dostawcami
-            usług marketingówych czy osobami współpracującymi, w celach
-            usprawnienia Strony i doświadczenia które dostarczamy klientom
-            online. 3.2 Oświadczasz, ze mozemy współpracować z zewnętrznymi
-            dostawcami / sprzedawcami lub zewnętrznym oprogramowaniem aby
-            reklamować lub tworzyć i uzywac kampanii marketingowych na tej
-            Stronie i poza nią. Jednocześnie oświadczając zgodę na to, Ci
-            zewnętrzni dostawcy równiez mogą zbierać Twoje dane i je
-            wykorzystywać w celu usprawnienia tej Strony. A tym samym jesteś
-            odpowiedzialny za zapoznanie się z ich polityką prywatności i
-            skontaktowanie się z nimi jeśli masz jakiekolwiek wątpliwości.
-        </p>
-        <p>
-            <strong>4. Bezpieczeństwo informacji</strong><br />
-            Uzyjemy rozsądynch środków i kroków aby zabezpieczyć i zarządzać
-            Twoimi danymi które zebraliśmy i otrzymaliśmy od Ciebie. Pomimo
-            naszych starań nie mozemy zagwarantowac bezpieczeństwa i prywatności
-            Twoich danych, zadne środki bezpieczeństwa nie są nieprzenikalny
-            przed jakimkolwiek przechwyceniem lub innym rodzajem niewłaściwego
-            użytkowania.
-        </p>
-        <p>
-            <strong>5. Polityka ciasteczek</strong><br />
-            Mozemy uzywac ciasteczek, web beacon'ów i innych technologii
-            śledzących na tej Stronie, które domyślnie są ustawione na
-            akceptacje ich ciasteczek, w celu polepszenia doświadczenia online
-            dla naszych klientów. Musisz być świadomym tego, ze blokowanie
-            ciasteczek lub web beacon'ów moze wpłynąć na funkcjonalność tej
-            Strony.
-        </p>
-        <p>
-            <strong>6. Treść stron trzecich</strong><br />
-            Jakakolwiek informacje, które dostarczysz stronom trzecim - nie
-            związanych z nami - nie są pokryte w tej Polityce. Nie jesteśmy
-            odpowiedzialni za zawartość, prywatność i bezpieczeństwo polityki
-            jakiejkolwiek ze stron trzecich, włączajac w to inne Strony
-            internetowe, reklamy, zewnętrzne seerwisy lub aplikacje, które mogą
-            być powiazane do tej Strony.
-        </p>
-        <p>
-            <strong>7. Ograniczenie odpowiedzialności</strong><br />
-
-            Nie jesteśmy odpowiedzialni za działalność stron trzecich z, którym
-            udostępnileś lub wymieniłeś swoje dane w jakiej kolwiek formie i nie
-            mamy mozliwosci zarządzania lub kontrolowania ich.
-        </p>
-        <p>
-            <strong>8. Komunikacja</strong><br />
-            Jeśli nie zyczysz sobie otrzymywania jakichkolwiek informacji od
-            nas, mozesz zrezygnować poprzez skontaktowanie się z nami poprzez
-            email lub jakąkolwiek inną formę kontaktu która jest zawarta na tej
-            Stronie.
-        </p>
-        <p>
-            <strong>9. Modyfikacja polityki prywatności</strong><br />
-            Przyjmujesz do wiadomości, ze ta polityka prywatności jest obiektem
-            modyfikacji, alternacji i poprawek przez
-            {{ name }} przez cały czas, bez lub z powiadomieniem Ciebie o nich.
-            Zgadzasz się równiez z tym, ze jakiekolwiek modyfikacje są w wazne i
-            mają wpływ po ich zamieszczeniu w tej Witrynie.
-        </p>
-    </div>
-    <Footer />
 </template>
 
 <script>
 import { Footer } from "../containers";
-export default { name: "Prywatnosc", components: { Footer } };
+
+let name = import.meta.env.VITE_NAME;
+let email = import.meta.env.VITE_EMAIL;
+let phone = import.meta.env.VITE_PHONE;
+let fb = import.meta.env.VITE_FB;
+let ig = import.meta.env.VITE_IG;
+
+export default {
+    name: "Prywatnosc",
+    data() {
+        return {
+            name: name,
+            email: email,
+            phone: phone,
+            fb: fb,
+            ig: ig,
+        };
+    },
+    components: { Footer },
+};
 </script>
