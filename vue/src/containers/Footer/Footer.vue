2eca8d3fcbfef0317c0178926ad0ee4004ee4ff0
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-06 23:55:33 +0200

    fix: Footer clip lineheight

--- vue/src/containers/Footer/Footer.vue
+++ vue/src/containers/Footer/Footer.vue
@@ -4,26 +4,18 @@
             <hr class="section-divider mt-6 mb-10" />
             <section>
                 <div class="container text-center md:text-left">
-                    <LinkPrimary
-                        text="Zostańmy w kontakcie"
-                        class="font-family-header contact cursor-pointer"
-                        :point="true"
-                        :split="true"
-                        to="Kontakt"
-                    />
+                    <div class="clip">
+                        <LinkPrimary text="Zostańmy w kontakcie"
+                            class="font-family-header contact cursor-pointer inline-block" :point="true" :split="true"
+                            to="Kontakt" />
+                    </div>
 
                     <div>
                         <div>
-                            <LinkPrimary
-                                :href="`mailto:${email}?subject = Oferta&body = Wiadomość`"
-                                :text="email"
-                                :primary="true"
-                                class="mail-link fw-medium font-color-secondary"
-                            />
+                            <LinkPrimary :href="`mailto:${email}?subject = Oferta&body = Wiadomość`" :text="email"
+                                :primary="true" class="mail-link fw-medium font-color-secondary" />
 
-                            <h4
-                                class="phone-number font-color-dark font-medium mb-2 mt-2"
-                            >
+                            <h4 class="phone-number font-color-dark font-medium mb-2 mt-2">
                                 {{ phone }}
                             </h4>
 
@@ -32,57 +24,31 @@
                             <div class="footer-links uppercase mt-12 fw-light">
                                 <ul class="space-y-2">
                                     <li>
-                                        <LinkPrimary
-                                            to="Start"
-                                            text="start"
-                                            :secondary="true"
-                                        />
+                                        <LinkPrimary to="Start" text="start" :secondary="true" />
                                     </li>
                                     <li>
-                                        <LinkPrimary
-                                            to="Oferta"
-                                            text="oferta"
-                                            :secondary="true"
-                                        />
+                                        <LinkPrimary to="Oferta" text="oferta" :secondary="true" />
                                     </li>
                                     <li>
-                                        <LinkPrimary
-                                            to="Portfolio"
-                                            text="portfolio"
-                                            :secondary="true"
-                                        />
+                                        <LinkPrimary to="Portfolio" text="portfolio" :secondary="true" />
                                     </li>
                                     <li>
-                                        <LinkPrimary
-                                            to="Kontakt"
-                                            text="kontakt"
-                                            :secondary="true"
-                                        />
+                                        <LinkPrimary to="Kontakt" text="kontakt" :secondary="true" />
                                     </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
 
-                    <div
-                        class="font-family-header uppercase text-center md:text-left mt-14 space-y-2 md:space-y-0"
-                    >
+                    <div class="font-family-header uppercase text-center md:text-left mt-14 space-y-2 md:space-y-0">
                         <div class="block md:inline-block mr-0 md:mr-4">
-                            <LinkPrimary
-                                to="Prywatnosc"
-                                text="Polityka prywatności"
-                                :secondary="true"
-                                class="font-color-dark"
-                            />
+                            <LinkPrimary to="Prywatnosc" text="Polityka prywatności" :secondary="true"
+                                class="font-color-dark" />
                         </div>
                         <ToggleThree class="md:inline-block" />
-                        <div
-                            class="block md:inline-block float-none md:float-right"
-                        >
-                            <span class="font-color-darker"
-                                >© 2021 {{ name }} wszelkie prawa
-                                zastrzeżone.</span
-                            >
+                        <div class="block md:inline-block float-none md:float-right">
+                            <span class="font-color-darker">© 2021 {{ name }} wszelkie prawa
+                                zastrzeżone.</span>
                         </div>
                     </div>
                 </div>
