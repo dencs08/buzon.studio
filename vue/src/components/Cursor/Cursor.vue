014525fff5c7ed33c4973e2689e53294a31fb299
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-08 07:53:56 +0200

    update: Cursor circle text

--- vue/src/components/Cursor/Cursor.vue
+++ vue/src/components/Cursor/Cursor.vue
@@ -1,10 +1,28 @@
 <template>
     <div>
-        <div ref="cursor" class="cursor absolute grid content-center rounded-full pointer-events-none select-none z-[10000]"
-             :class="{'active' : isMouseOver}">
+        <div ref="cursor"
+            class="cursor absolute grid content-center rounded-full pointer-events-none select-none z-[10000]"
+            :class="{'active' : isMouseOver}">
         </div>
-        <div ref="label" class="label absolute text-center uppercase font-bold pointer-events-none select-none z-[10001]" :class="{'active' : isMouseOver}">
-            <span v-show="dataCursorLabel">{{dataCursorLabel}}</span>
+        <div ref="label"
+            class="label absolute text-center uppercase font-bold pointer-events-none select-none z-[10001]"
+            :class="{'active' : isMouseOver}">
+            <div id="circle" class="">
+                <svg version=1.1 xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink x=0px y=0px
+                    width=300px height=300px viewBox="0 0 300 300" enable-background="new 0 0 300 300"
+                    xml:space=preserve>
+                    <defs>
+                        <path id=circlePath d="M 150, 150 m -65, 0 a 35,35 0 0,1 130,0 a 65,65 0 0,1 -130,0 " />
+                    </defs>
+                    <circle cx=150 cy=100 r=75 fill=none />
+                    <g>
+                        <use xlink:href="#circlePath" fill=none />
+                        <text>
+                            <textPath xlink:href="#circlePath">• {{dataCursorLabel}} • {{dataCursorLabel}}</textPath>
+                        </text>
+                    </g>
+                </svg>
+            </div>
         </div>
     </div>
 </template>
@@ -19,7 +37,7 @@ export default {
     setup() {
         let cursor, label, arrayOfElements, dataCursorLabel;
 
-        return { cursor, label, arrayOfElements,dataCursorLabel }
+        return { cursor, label, arrayOfElements, dataCursorLabel }
     },
     mounted() {
         this.cursor = this.$refs.cursor;
@@ -37,25 +55,25 @@ export default {
             setTimeout(() => {
                 this.arrayOfElements = document.querySelectorAll('[data-cursor]');
                 this.attachListeners();
-            },[] )
+            }, [])
         },
 
         attachListeners() {
             let _this = this;
             this.arrayOfElements.forEach(element => {
                 element.addEventListener("mouseover", function () {
-                    if(_this.isMouseOver) return;
+                    if (_this.isMouseOver) return;
                     _this.isMouseOver = true
                     _this.dataCursorLabel = element.getAttribute("data-cursor")
                 });
                 element.addEventListener("mouseleave", function () {
-                    if(!_this.isMouseOver) return;
+                    if (!_this.isMouseOver) return;
                     _this.isMouseOver = false
                 });
             });
         },
 
-        restartCursor(){
+        restartCursor() {
             this.isMouseOver = false;
         },
 
