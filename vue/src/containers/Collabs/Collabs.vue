6f77e72887e3eb1f6ccef73e7d0ad53c1e6206cb
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-11 17:08:59 +0200

    update: Collabs.vue responsivness

--- vue/src/containers/Collabs/Collabs.vue
+++ vue/src/containers/Collabs/Collabs.vue
@@ -3,13 +3,13 @@
         <div class="container">
             <h4 class="mb-2">Oni juz nam zaufali:</h4>
             <div
-                class="xs:[&>*]:px-3 xl:[&>*]:px-12 space-y-4 xs:space-y-0 flex xs:flex-row flex-col flex-wrap justify-center items-center md:flex-nowrap text-center">
+                class="[&>*]:px-3 xl:[&>*]:px-12 xs:space-y-0 flex flex-wrap justify-center items-center md:flex-nowrap text-center">
                 <CollabItem name="danfit" class="grow" />
                 <CollabItem name="komb" class="grow" />
-                <CollabItem name="zielonewidoki" class="grow" />
-                <CollabItem name="aviorprojekt" class="grow" />
-                <CollabItem name="ats" class="grow" />
-                <CollabItem name="bielanypark" class="grow" />
+                <CollabItem name="zielonewidoki" class="grow hidden sm:block" />
+                <CollabItem name="ats" class="grow hidden md:block" />
+                <CollabItem name="bielanypark" class="grow hidden md:block" />
+                <CollabItem name="aviorprojekt" class="grow hidden lg:block" />
             </div>
         </div>
     </section>
