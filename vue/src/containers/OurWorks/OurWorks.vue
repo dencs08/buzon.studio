2b159c86f59a4b0dbb042b9b17fad1838bb3b28b
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-11 17:09:23 +0200

    update: OurWorks.vue scrub animations

--- vue/src/containers/OurWorks/OurWorks.vue
+++ vue/src/containers/OurWorks/OurWorks.vue
@@ -1,9 +1,9 @@
 <template>
-    <section id="portfolio">
+    <section id="portfolio" class="overflow-hidden">
         <div id="portfolio-content" class="container mt-5">
-            <SectionHeader headerLine="Nasze prace" mainHeader="Projekty z, których jesteśmy dumni" class="mb-5" />
+            <h3 ref="header" class="mb-5 uppercase font-bold sectionHeader absolute top-[-20%] z-[-1]">Projekty</h3>
         </div>
-        <OurWorksSlider />
+        <OurWorksSlider ref="ourWorksSlider" />
         <div class="portfolio-container container">
             <LinkPrimary to="Portfolio" text="Więcej prac" :primary="true" :point="true" class="my-4 uppercase" />
         </div>
@@ -13,11 +13,33 @@
 <script>
 import { LinkPrimary, SectionHeader, OurWorksSlider } from "../../components";
 
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
 export default {
     components: {
         OurWorksSlider,
         LinkPrimary,
         SectionHeader,
+    },
+
+    mounted() {
+        setTimeout(() => {
+            if (!window.matchMedia("(min-width: 1024px)").matches) return;
+            console.log(window.innerWidth);
+            let tlScroll = gsap.timeline({ defaults: { ease: "none" } })
+                .fromTo(this.$refs.header, { x: 0 }, { x: "-110%", duration: 20 })
+                .fromTo(this.$refs.ourWorksSlider.$el, { x: '101%' }, { x: "-200%", duration: 17 }, 12)
+            ScrollTrigger.create({
+                trigger: "#portfolio",
+                start: "40% 50%",
+                end: "+=5000",
+                scroller: "main",
+                animation: tlScroll,
+                scrub: true,
+                pin: true,
+            })
+        }, 200);
     },
 };
 </script>
