aff4596fc862f0bb5110d0bb708a85edcc51c2fc
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-12 15:38:01 +0200

    update: Hero.vue gsap improve

--- vue/src/containers/Hero/Hero.vue
+++ vue/src/containers/Hero/Hero.vue
@@ -10,11 +10,11 @@
                 </div>
             </h1>
 
-            <div class="w-1/3 mt-24 px-4">
-                <h2 ref="h2" class="fw-normal font-family-primary relative">
+            <div class="mt-24 px-4">
+                <h2 id="heroH2" ref="h2" class="fw-normal font-family-primary relative">
                     <LinkParagraph to="Oferta" @mouseover="$refs.link2.animationStart()"
                         @mouseleave="$refs.link2.handleMouseLeave()">
-                        <span ref="span" class="font-color-darker duration-200 kern-0 test">
+                        <span ref="span" :class="{'opacity-0' : isSplit}" class="font-color-darker duration-200 kern-0">
                             Tworzymy strony internetowe, wizualizacje, branding,
                             marketing online i inne produkty cyfrowe z pomocą
                             metody</span>
@@ -46,52 +46,61 @@ export default {
         LinkParagraph,
     },
 
-    setup() {
-        let h1;
+    data() {
         return {
-            h1
+            isSplit: true,
+        }
+    },
+
+    setup() {
+        let h1, words;
+        return {
+            h1, words
         }
     },
 
     mounted() {
         this.h1 = this.$refs.h1;
 
-        gsap.registerPlugin(ScrollTrigger)
-
         const elementToSplit = this.$refs.span
         splitToWords(elementToSplit);
+        this.isSplit = false;
+        this.words = document.querySelectorAll("#heroH2 .word")
 
-        gsap.to(this.$refs.h1.children[0].children, { y: "0", delay: 0.3, ease: "expo.out" })
-        gsap.to(this.$refs.h1.children[1].children, { y: "0", delay: 0.3, ease: "expo.out" })
-        gsap.to(".word", { y: "0", stagger: 0.02, delay: 0.3, ease: "expo.out" })
-        gsap.to(this.$refs.link2.$el, { opacity: "1", delay: 0.8, duration: 1.5, ease: "expo.out" })
+        let tl = gsap.timeline({ defaults: { ease: "expo.out" } });
+        tl.to(this.$refs.h1.children[0].children, { y: "0", delay: 1, })
+            .to(this.$refs.h1.children[1].children, { y: "0", }, "<0.25")
+            .to(this.words, { y: "0", stagger: 0.02 })
+            .to(this.$refs.link2.$el, { opacity: "1", ease: "power2.in", onComplete: () => { this.scrollTriggerAnimations() } }, ">-0.5")
 
-
-        setTimeout(() => {
-            let tlScroll = gsap.timeline({ defaults: { ease: "none" } })
-                .fromTo(this.$refs.h1.children[0], { x: 0 }, { x: "-120%", duration: 5 })
-                .fromTo(this.$refs.h1.children[1], { x: 0 }, { x: "120%", duration: 3.8 }, 0)
-                .fromTo(".word", { y: 0 }, {
-                    duration: 3, y: "-100%", stagger: 0.05,
-                }, 0)
-                .fromTo(this.$refs.link2.$el.children, { y: 0 }, { y: "-110%", duration: 2.5 }, 0.8)
-
-            ScrollTrigger.create({
-                trigger: "#hero",
-                start: "55% 50%",
-                end: "+=1000",
-                scroller: "main",
-                animation: tlScroll,
-                scrub: true,
-                pin: true,
-            })
-        }, 200);
 
     },
 
     methods: {
         mouseOver() {
         },
+
+        scrollTriggerAnimations() {
+            gsap.registerPlugin(ScrollTrigger)
+
+            let tlScroll = gsap.timeline({ defaults: { ease: "none" } })
+                .fromTo(this.$refs.h1.children[0], { x: 0 }, { x: "-120%", duration: 5 })
+                .fromTo(this.$refs.h1.children[1], { x: 0 }, { x: "120%", duration: 3.8 }, 0)
+                .fromTo(this.words, { y: 0 }, {
+                    duration: 3, y: "-100%", stagger: 0.05,
+                }, 0)
+                .fromTo(this.$refs.link2.$el.children, { y: 0 }, { y: "-110%", duration: 2.5 }, 0.8)
+
+            ScrollTrigger.create({
+                trigger: "#hero",
+                start: "56% 50%",
+                end: "+=1000",
+                scroller: "main",
+                animation: tlScroll,
+                scrub: true,
+                pin: true,
+            })
+        }
     },
 };
 </script>
