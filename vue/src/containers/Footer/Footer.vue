631dff53755aa456f5dfe2ee2f3fb5671985053e
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-26 14:20:31 +0200

    update: footer responsvines

--- vue/src/containers/Footer/Footer.vue
+++ vue/src/containers/Footer/Footer.vue
@@ -2,12 +2,12 @@
     <footer id="footer" class="mb-2 relative">
         <section class="container">
             <hr class="section-divider mt-6 mb-10 w-full" />
-            <div class="grid grid-cols-4">
+            <div class="block md:grid grid-cols-4">
                 <div class="col-span-3">
-                    <div class="mb-12">
+                    <div class="mb-6 md:mb-12">
                         <h4 class="mb-4">Masz pomysł na projekt?</h4>
-                        <LinkPrimary text="Opowiedz nam o nim" class="contact-line cursor-pointer inline-block w-7/12"
-                            :point="true" to="Kontakt" />
+                        <LinkPrimary text="Opowiedz nam o nim"
+                            class="contact-line cursor-pointer inline-block md:w-7/12" :point="true" to="Kontakt" />
                     </div>
 
                     <div class="footer-socials space-y-2">
@@ -43,16 +43,20 @@
 
                     </div>
 
-                    <div class="mt-20">
-                        <div
-                            class="goUp w-[100px] h-[100px] grid content-center rounded-full border-2 border-zinc-700 font-color-darker">
-                            <div class="mx-auto">GoUp</div>
-                        </div>
+                    <div class="md:mt-20">
+                        <a href="">
+                            <div
+                                class="hidden md:block goUp w-[100px] h-[100px] rounded-full border-2 border-zinc-700 font-color-darker">
+                                <div class="w-full h-full grid content-center">
+                                    <Icon icon="ri:arrow-up-line" class="mx-auto" width="30px" />
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
 
-                <div class="flex flex-col items-end justify-end">
-                    <div class="footer-links uppercase mt-12 fw-light text-right">
+                <div class="block md:flex flex-col items-end justify-end md:text-right">
+                    <div class="footer-links uppercase mt-4 md:mt-12 mb-4 md:mb-0 fw-light">
                         <ul class="space-y-2 font-color-dark">
                             <li>
                                 <LinkPrimary to="Start" text="start" :secondary="true" />
@@ -69,27 +73,27 @@
                         </ul>
                     </div>
 
-                    <hr class="section-divider w-64 my-4 ml-auto" />
+                    <hr class="section-divider w-64 my-4 md:ml-auto hidden md:block" />
 
-                    <div class="font-family-header uppercase text-right font-color-darker">
+                    <div class="font-family-header uppercase font-color-darker space-y-2 md:space-y-3">
                         <div class="">
-                            <LinkPrimary to="Prywatnosc" text="Polityka prywatności" :secondary="true" />
+                            <ToggleThree class="font-color-darker" />
                         </div>
 
-                        <hr class="section-divider w-64 my-4 ml-auto" />
+                        <hr class="section-divider w-64 my-4 md:ml-auto hidden md:block" />
 
-                        <ToggleThree class="font-color-darker" />
+                        <LinkPrimary to="Prywatnosc" text="Polityka prywatności" :secondary="true" />
 
-                        <hr class="section-divider w-64 my-4 ml-auto" />
+                        <hr class="section-divider w-64 my-4 md:ml-auto hidden md:block" />
 
                         <div class="">
                             <span>© 2021 {{ name }} wszelkie prawa
                                 zastrzeżone.</span>
                         </div>
 
-                        <hr class="section-divider w-64 my-4 ml-auto" />
+                        <hr class="section-divider w-64 my-4 md:ml-auto hidden md:block" />
 
-                        <div>
+                        <div class="hidden md:block">
                             <LinkPrimary to="Start" text="BUZON.STUDIO" />
                         </div>
 
