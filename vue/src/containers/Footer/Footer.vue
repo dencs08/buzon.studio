9d01230d2ef8e6659d8a388ae8fc025cde69b1bf
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-26 13:30:16 +0200

    update: footer layout change

--- vue/src/containers/Footer/Footer.vue
+++ vue/src/containers/Footer/Footer.vue
@@ -1,58 +1,95 @@
 <template>
     <footer id="footer" class="mb-2 relative">
-        <section>
-            <hr class="section-divider mt-6 mb-10" />
-            <section>
-                <div class="container text-center md:text-left">
-                    <div class="clip">
-                        <LinkPrimary text="Zostańmy w kontakcie"
-                            class="font-family-header contact cursor-pointer inline-block" :point="true" :split="true"
-                            to="Kontakt" />
+        <section class="container">
+            <hr class="section-divider mt-6 mb-10 w-full" />
+            <div class="grid grid-cols-4">
+                <div class="col-span-3">
+                    <div class="mb-12">
+                        <h4 class="mb-4">Masz pomysł na projekt?</h4>
+                        <LinkPrimary text="Opowiedz nam o nim" class="contact-line cursor-pointer inline-block w-7/12"
+                            :point="true" to="Kontakt" />
                     </div>
 
-                    <div>
+                    <div class="footer-socials space-y-2">
+                        <LinkPrimary :href="`mailto:${email}?subject = Oferta&body = Wiadomość`" :text="email"
+                            class="font-color-dark" />
+
+                        <hr class="section-divider w-2/6 my-4 mr-auto" />
+
+                        <h4 class="font-color-dark">
+                            {{ phone }}
+                        </h4>
+
+                        <hr class="section-divider w-2/6 my-4 mr-auto" />
+
                         <div>
-                            <LinkPrimary :href="`mailto:${email}?subject = Oferta&body = Wiadomość`" :text="email"
-                                :primary="true" class="mail-link fw-medium font-color-secondary" />
-
-                            <h4 class="phone-number font-color-dark font-medium mb-2 mt-2">
-                                {{ phone }}
-                            </h4>
-
-                            <SocialIcons class="space-x-3 mt-5" />
-
-                            <div class="footer-links uppercase mt-12 fw-light">
-                                <ul class="space-y-2">
-                                    <li>
-                                        <LinkPrimary to="Start" text="start" :secondary="true" />
-                                    </li>
-                                    <li>
-                                        <LinkPrimary to="Oferta" text="oferta" :secondary="true" />
-                                    </li>
-                                    <li>
-                                        <LinkPrimary to="Portfolio" text="portfolio" :secondary="true" />
-                                    </li>
-                                    <li>
-                                        <LinkPrimary to="Kontakt" text="kontakt" :secondary="true" />
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="font-family-header uppercase text-center md:text-left mt-14 space-y-2 md:space-y-0">
-                        <div class="block md:inline-block mr-0 md:mr-4">
-                            <LinkPrimary to="Prywatnosc" text="Polityka prywatności" :secondary="true"
+                            <LinkPrimary :href="fb" rel="noreferrer" target="_blank" text="Facebook"
                                 class="font-color-dark" />
                         </div>
-                        <ToggleThree class="md:inline-block" />
-                        <div class="block md:inline-block float-none md:float-right">
-                            <span class="font-color-darker">© 2021 {{ name }} wszelkie prawa
-                                zastrzeżone.</span>
+
+                        <hr class="section-divider w-2/6 my-4 mr-auto" />
+
+                        <div>
+                            <LinkPrimary :href="ig" rel="noreferrer" target="_blank" text="Instagram"
+                                class="font-color-dark" />
+                        </div>
+                        <hr class="section-divider w-2/6 my-4 mr-auto" />
+
+                    </div>
+
+                    <div class="mt-20">
+                        <div
+                            class="goUp w-[100px] h-[100px] grid content-center rounded-full border-2 border-zinc-700 font-color-darker">
+                            <div class="mx-auto">GoUp</div>
                         </div>
                     </div>
                 </div>
-            </section>
+
+                <div class="flex flex-col items-end justify-end">
+                    <div class="footer-links uppercase mt-12 fw-light text-right">
+                        <ul class="space-y-2 font-color-dark">
+                            <li>
+                                <LinkPrimary to="Start" text="start" :secondary="true" />
+                            </li>
+                            <li>
+                                <LinkPrimary to="Oferta" text="oferta" :secondary="true" />
+                            </li>
+                            <li>
+                                <LinkPrimary to="Portfolio" text="portfolio" :secondary="true" />
+                            </li>
+                            <li>
+                                <LinkPrimary to="Kontakt" text="kontakt" :secondary="true" />
+                            </li>
+                        </ul>
+                    </div>
+
+                    <hr class="section-divider w-64 my-4 ml-auto" />
+
+                    <div class="font-family-header uppercase text-right font-color-darker">
+                        <div class="">
+                            <LinkPrimary to="Prywatnosc" text="Polityka prywatności" :secondary="true" />
+                        </div>
+
+                        <hr class="section-divider w-64 my-4 ml-auto" />
+
+                        <ToggleThree class="font-color-darker" />
+
+                        <hr class="section-divider w-64 my-4 ml-auto" />
+
+                        <div class="">
+                            <span>© 2021 {{ name }} wszelkie prawa
+                                zastrzeżone.</span>
+                        </div>
+
+                        <hr class="section-divider w-64 my-4 ml-auto" />
+
+                        <div>
+                            <LinkPrimary to="Start" text="BUZON.STUDIO" />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
             <div class="scroll-blocker"></div>
         </section>
     </footer>
@@ -62,6 +99,9 @@
 let name = import.meta.env.VITE_NAME;
 let email = import.meta.env.VITE_EMAIL;
 let phone = import.meta.env.VITE_PHONE;
+let fb = import.meta.env.VITE_FB;
+let ig = import.meta.env.VITE_IG;
+
 
 import { LinkPrimary, SocialIcons, ToggleThree } from "../../components";
 
@@ -71,6 +111,8 @@ export default {
             name: name,
             email: email,
             phone: phone,
+            fb: fb,
+            ig: ig,
         };
     },
 
