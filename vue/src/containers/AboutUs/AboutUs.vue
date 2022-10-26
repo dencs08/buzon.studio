384640ae85897636562176118e2739117b38f971
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-26 13:31:31 +0200

    add: AboutUs link Kontakt

--- vue/src/containers/AboutUs/AboutUs.vue
+++ vue/src/containers/AboutUs/AboutUs.vue
@@ -4,7 +4,9 @@
             <p class="font-color-dark" ref="text">
                 <b class="uppercase font-bold">{{ name }}</b> to 5 lat
                 doświadczenia w tworzeniu projektów i pomaganiu markom w
-                optymalizacji ich działań marketingowych. Stwórzmy coś razem!
+                optymalizacji ich działań marketingowych.
+
+                <LinkPrimary to="Kontakt" text="Stwórzmy coś razem!" :primary="true" class="font-medium" />
             </p>
         </div>
     </section>
@@ -19,35 +21,30 @@ import { splitToLines } from "../../js/cloneSplit"
 
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
+import LinkPrimary from '../../components/CallToAction/Links/LinkPrimary/LinkPrimary.vue';
 
 export default {
+    components: { LinkPrimary },
     data() {
         return {
             name: name,
         };
     },
-
     mounted() {
         let text = this.$refs.text;
         let section = this.$refs.section;
-
         // setTimeout(() => {
         //     scrubTextReveal(text, this.$refs.section)
         // }, 1000);
-
         setTimeout(() => {
             splitToLines(text);
             let lines = text.querySelectorAll(".line");
-
-            gsap.registerPlugin(ScrollTrigger)
-
+            gsap.registerPlugin(ScrollTrigger);
             let tlScroll = gsap.timeline({ defaults: { ease: "none" }, paused: true });
             tlScroll
                 .fromTo(lines, { y: "100%" }, { y: "0%", duration: 1, stagger: 0.05 })
-                .to(lines, { y: "0", duration: .75, stagger: 0.05 })
-                .to(lines, { y: "-100%", duration: 1, stagger: 0.05 })
-
-
+                .to(lines, { y: "0", duration: 0.75, stagger: 0.05 })
+                .to(lines, { y: "-100%", duration: 1, stagger: 0.05 });
             ScrollTrigger.create({
                 trigger: section,
                 start: "50% 50%",
@@ -56,9 +53,9 @@ export default {
                 animation: tlScroll,
                 scrub: 1,
                 pin: true,
-            })
-        }, 1000)
-    }
+            });
+        }, 1000);
+    },
 };
 </script>
 
