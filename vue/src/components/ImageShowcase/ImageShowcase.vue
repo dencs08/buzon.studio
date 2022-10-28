96780859f82b69424608ae086b9ac58166ac43f8
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-28 16:52:15 +0200

    add: ImageShowcase horizontal boolean

--- vue/src/components/ImageShowcase/ImageShowcase.vue
+++ vue/src/components/ImageShowcase/ImageShowcase.vue
@@ -1,9 +1,10 @@
 <template>
     <router-link :to="'/portfolio/' + title" @mouseover="mouseOver" @mouseleave="mouseLeave">
         <div class="portfolio-wrapper relative whitespace-normal">
-            <div class="portfolio-item mx-auto flex inline-block overflow-hidden">
+            <div class="portfolio-item mx-auto flex inline-block overflow-hidden h-auto "
+                :class="[horizontal ? 'aspect-[7/10]' : 'aspect-[10/7]']">
                 <img :src="image" alt="" class="w-full h-full object-cover duration-500 z-[-1]" :class="{
-                    'scale-[1.2] brightness-110 saturate-[1.1]':
+                    'scale-[1.1] brightness-110 saturate-[1.1]':
                         this.isMouseOver,
                 }" />
             </div>
@@ -11,10 +12,10 @@
             <div class="info ml-4 sm:ml-8 lg:ml-10">
                 <div class="font-family-header my-0">
                     <ItemHeader :text="title" :big="true" :split="true"
-                        headerClass="mt-[-15px] md:mt-[-20px] lg:mt-[-30px] xl:mt-[-40px] z-[2] inline-block"
+                        headerClass="mt-[-15px] md:mt-[-20px] lg:mt-[-30px] xl:mt-[-50px] z-[2] inline-block"
                         ref="itemHeader" />
                 </div>
-                <p class="font-color-secondary mb-1 md:mb-3 xl:mb-6">
+                <p class="font-color-secondary mb-1 md:mb-3 xl:mb-6 break-all pr-5 lg:pr-10">
                     {{ info }}
                 </p>
                 <span v-for="item in category"
@@ -40,6 +41,7 @@ export default {
         title: String,
         info: String,
         category: Array,
+        horizontal: Boolean,
     },
 
     computed: {
