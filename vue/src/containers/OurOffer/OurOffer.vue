d71d470ef549afe26ac57d631cc05386e6e88a61
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-10-31 00:36:06 +0100

    add: OurOffer

---
+++ vue/src/containers/OurOffer/OurOffer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <OurOfferColumn title="Produkty cyfrowe"
+            paragraph="Tworzymy i rozwijamy, strony internetowe, aplikacje, wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje potrzeby, tak abyś otrzymał najlepiej dostosowany produkt który pracuje dla Ciebie."
+            ul=" · Strony internetowe · E-commerce · UI / UX · Projekty graficzne · Wizualizacje 3D · Aplikacje ·"
+            :buttons="buttons" />
+        <OurOfferColumn title="Branding"
+            paragraph="Dziś branding jest najważniejszym elementem każdego dobrze prosperującego biznesu, łatwe do zapamiętania logo, charakterystyczna identyfikacja wizualna czy dobrze obrana strategia marketingowa, ma ogromne znaczenie w jaki sposób będziesz odbierany na rynku"
+            ul=" · Nazewnictwo · Identyfikacja wizualna · Logotypy · Marketing · Strategie biznesowe w social mediach ·"
+            :buttons="buttons" />
+    </div>
+</template>
+<script>
+import { OurOfferColumn } from '../../components'
+export default {
+    props: {
+        buttons: Boolean,
+    },
+
+    components: {
+        OurOfferColumn,
+    }
+}
+</script>
+<style lang="">
+    
+</style>
