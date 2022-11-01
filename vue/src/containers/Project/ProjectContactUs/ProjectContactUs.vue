95730db72cac99fdfca61ae128fed9a569b71400
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-01 19:09:15 +0100

    add: ProjectContactUs container

---
+++ vue/src/containers/Project/ProjectContactUs/ProjectContactUs.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <div class="container">
+            <h5 class="font-color-darker">Podobają Ci się nasze prace?</h5>
+            <div class="link">
+                <LinkPrimary text="Zacznijmy współpracę" to="Kontakt" :primary="true" :point="true" />
+            </div>
+            <AutoTextSlider text="BUZON.STUDIO" :wordAmount="3" fontSize="12vw" height="10vw" class="my-6 md:my-10" />
+        </div>
+    </section>
+</template>
+
+<script>
+import { LinkPrimary, AutoTextSlider } from '../../../components'
+export default {
+    components: {
+        LinkPrimary,
+        AutoTextSlider
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h5 {
+    font-size: clamp(0.9rem, 1.25vw, 5rem);
+}
+
+.link {
+    font-size: clamp(0.9rem, 3vw, 5rem);
+
+}
+</style>
