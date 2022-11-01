c2c24fad58442b136235fb77ff0628f78c1284f7
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-01 19:10:23 +0100

    update: OurOffer.vue html structure simplified

--- vue/src/containers/OurOffer/OurOffer.vue
+++ vue/src/containers/OurOffer/OurOffer.vue
@@ -1,19 +1,19 @@
 <template>
     <div id="OurOffer">
-        <div class="mt-[6vh]">
-            <OurOfferColumn title="Produkty cyfrowe"
-                paragraph="Tworzymy i rozwijamy, strony internetowe, aplikacje, wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje potrzeby, tak abyś otrzymał najlepiej dostosowany produkt który pracuje dla Ciebie."
-                :text="['Strony internetowe', 'E-commerce', 'Aplikacje']"
-                :textSecond="['UI/UX', 'Projekty graficzne', 'Wizualizacje 3D']" :to="to"
-                :bottomLinkText="bottomLinkText" :buttons="buttons" />
-        </div>
-        <div class="mt-[6vh]">
-            <OurOfferColumn title="Branding"
-                paragraph="Dziś branding jest najważniejszym elementem każdego dobrze prosperującego biznesu, łatwe do zapamiętania logo, charakterystyczna identyfikacja wizualna czy dobrze obrana strategia marketingowa, ma ogromne znaczenie w jaki sposób będziesz odbierany na rynku"
-                :text="['Nazewnictwo', 'Identyfikacja wizualna', 'Logotypy']"
-                :textSecond="['Marketing', 'Socialmedia', 'Strategie biznesowe']" :to="to"
-                :bottomLinkText="bottomLinkText" :buttons="buttons" />
-        </div>
+
+        <OurOfferColumn title="Produkty cyfrowe"
+            paragraph="Tworzymy i rozwijamy, strony internetowe, aplikacje, wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje potrzeby, tak abyś otrzymał najlepiej dostosowany produkt który pracuje dla Ciebie."
+            :text="['Strony internetowe', 'E-commerce', 'Aplikacje']"
+            :textSecond="['UI/UX', 'Projekty graficzne', 'Wizualizacje 3D']" :to="to" :bottomLinkText="bottomLinkText"
+            :buttons="buttons" />
+
+
+        <OurOfferColumn title="Branding"
+            paragraph="Dziś branding jest najważniejszym elementem każdego dobrze prosperującego biznesu, łatwe do zapamiętania logo, charakterystyczna identyfikacja wizualna czy dobrze obrana strategia marketingowa, ma ogromne znaczenie w jaki sposób będziesz odbierany na rynku"
+            :text="['Nazewnictwo', 'Identyfikacja wizualna', 'Logotypy']"
+            :textSecond="['Marketing', 'Socialmedia', 'Strategie biznesowe']" :to="to" :bottomLinkText="bottomLinkText"
+            :buttons="buttons" />
+
     </div>
 </template>
 <script>
