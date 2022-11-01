2d96dc48a7d053d3c44cd31fca2d38c09f3db01e
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-01 19:10:51 +0100

    update: OurOfferColumn new pin prop

--- vue/src/components/OurOfferColumn/OurOfferColumn.vue
+++ vue/src/components/OurOfferColumn/OurOfferColumn.vue
@@ -3,7 +3,7 @@
         <router-link :to="{ name: to }" @mouseover="mouseOver" @mouseleave="mouseLeave">
             <div class="offer-wrapper relative">
                 <div class="mb-10 md:mb-0 overflow-hidden">
-                    <div class="my-0 ml-0 sm:ml-2 md:ml-4">
+                    <div class="my-0 ml-0 sm:ml-2 md:ml-4 md:mt-28">
                         <ItemHeader :text="title" :big="true" :move="true" headerClass="inline-block uppercase"
                             ref="header" />
                         <div class="mt-3 pr-3 md:pr-0 ml-2 md:ml-0">
@@ -12,9 +12,9 @@
                             </p>
                             <Button text="Oferta" class="mt-6" :arrow="true" :secondary="true" v-if="buttons" />
                             <div>
-                                <ScrubTextSlider :text="text" fontSize="12vw" height="8vw" class="" />
-                                <ScrubTextSlider :text="textSecond" fontSize="12vw" height="8vw" class=""
-                                    :isReverse="true" />
+                                <ScrubTextSlider :text="text" fontSize="12vw" height="6.5vw" class="" />
+                                <ScrubTextSlider :text="textSecond" fontSize="12vw" height="6.5vw" class=""
+                                    :isReverse="true" :pin="true" />
                             </div>
                         </div>
                     </div>
