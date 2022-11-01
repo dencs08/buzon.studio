0c78a4128bb91fcc9f6837843f35e82e730ff722
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-01 19:11:35 +0100

    update: ScrubTextSlider pin & images props

--- vue/src/components/ScrubTextSlider/ScrubTextSlider.vue
+++ vue/src/components/ScrubTextSlider/ScrubTextSlider.vue
@@ -3,7 +3,8 @@
         :style="`height: ${height}`">
         <div ref="TextToAnimate" class="relative font-family-header flex items-center w-full whitespace-nowrap"
             :style="`height: ${height}`">
-            <div v-for="x in this.multiple">
+
+            <div v-if="text" v-for="x in this.multiple">
                 <span v-for="word in text" class="text-center whitespace-nowrap break-keep">
                     <span>
                         <span class="mx-5 text-highlight">·</span>
@@ -13,6 +14,18 @@
                     </span>
                 </span>
             </div>
+
+            <div v-if="images">
+                <div class="w-screen flex flex-row overflow-hidden relative">
+                    <div ref="imageWrapper" class="break-keep whitespace-nowrap space-x-10">
+                        <div v-for="image in newImagesArray"
+                            class="inline-block w-[44vw] h-auto aspect-[12/8] overflow-hidden">
+                            <img :src="imageSrc(image)" alt="" class="w-full h-full object-cover">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -24,49 +37,97 @@ import ScrollTrigger from 'gsap/ScrollTrigger'
 export default {
     props: {
         text: Array,
+        images: Array,
         fontSize: String,
         height: String,
+        pin: Boolean,
         isReverse: Boolean,
     },
 
     data() {
         let multiple = 2;
+        let tl, scrollTriggerStart, scrollTriggerEnd, scrollTriggerTrigger, newImagesArray;
 
-        return { multiple }
+        return { multiple, tl, scrollTriggerStart, scrollTriggerEnd, scrollTriggerTrigger, newImagesArray }
     },
 
     mounted() {
-        const textToAnimate = this.$refs.TextToAnimate;
-        const parent = this.$parent.$el.parentNode;
-        let tl = gsap.timeline({ defaults: { ease: 'none', duration: 5 } });
+        this.tl = gsap.timeline({ defaults: { ease: 'none', duration: 5 } });
 
-        if (this.isReverse) {
-            gsap.set(textToAnimate, { x: "-50%" });
-
-            tl.to(textToAnimate, {
-                x: "0%"
-            }, "<")
-        } else {
-            tl.to(textToAnimate, {
-                x: "-50%",
-            })
-        }
+        this.textCase()
+        this.imageCase()
 
         setTimeout(() => {
             gsap.registerPlugin(ScrollTrigger)
             ScrollTrigger.create({
-                trigger: parent,
-                start: "50% 50%",
-                end: "+=1500",
+                trigger: this.scrollTriggerTrigger,
+                start: this.scrollTriggerStart,
+                end: this.scrollTriggerEnd,
                 scroller: "#smoothScroll",
-                animation: tl,
+                animation: this.tl,
                 scrub: 0.3,
-                pin: this.isReverse,
+                pin: this.pin,
             })
         }, 1000);
-    }
+    },
+
+    methods: {
+        imageSrc(imageName) {
+            return `/src/assets/images/portfolio/bielanypark/${imageName}.jpg`;
+        },
+
+        textCase() {
+            if (!this.text) return;
+
+            const textToAnimate = this.$refs.TextToAnimate;
+            const parent = this.$parent.$el.parentNode;
+
+            this.scrollTriggerStart = "50% 50%"
+            this.scrollTriggerEnd = "+=1500"
+            this.scrollTriggerTrigger = parent;
+
+            if (this.isReverse) {
+                gsap.set(textToAnimate, { x: "-50%" });
+
+                this.tl.to(textToAnimate, {
+                    x: "0%"
+                }, "<")
+            } else {
+                this.tl.to(textToAnimate, {
+                    x: "-50%",
+                })
+            }
+        },
+
+        imageCase() {
+            if (!this.images) return;
+
+            const imageWrapper = this.$refs.imageWrapper;
+            let height = window.innerHeight * 2;
+
+            this.newImagesArray = this.images;
+            this.newImagesArray.push(...this.images);
+
+            this.scrollTriggerStart = "-10% bottom"
+            this.scrollTriggerEnd = `+=${height}`
+            this.scrollTriggerTrigger = this.$refs.ScrubTextSlider;
+
+
+            if (this.isReverse) {
+                gsap.set(imageWrapper, { x: "-50%" });
+
+                this.tl.to(imageWrapper, {
+                    x: "0%"
+                }, "<")
+            } else {
+                this.tl.to(imageWrapper, {
+                    x: "-50%",
+                })
+            }
+        }
+    },
 }
 </script>
 <style lang="scss">
-@import './scrubtextslider.scss'
+@import './scrubtextslider.scss';
 </style>
