539cfb7e3bccb233bc775cbb47e75bc8b9ac9b5a
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-01 19:08:44 +0100

    add: ProjectImageShowcase

---
+++ vue/src/containers/Project/ProjectImageShowcase/ProjectImageShowcase.vue
@@ -0,0 +1,24 @@
+<template>
+    <section id="ProjectImageShowcase" class="space-y-9 section-mb-s">
+        <ScrubTextSlider :images="['bielanypark3', 'bielanypark4', 'bielanypark5', 'bielanypark6', 'bielanypark7']" />
+        <ScrubTextSlider :images="['bielanypark7', 'bielanypark6', 'bielanypark4', 'bielanypark3', 'bielanypark5']"
+            :isReverse="true" />
+    </section>
+
+</template>
+<script>
+import { ScrubTextSlider } from '../../../components'
+export default {
+    components: {
+        ScrubTextSlider
+    },
+
+    mounted() {
+
+    },
+}
+</script>
+
+<style lang="scss">
+@import './projectshowcaseimage.scss';
+</style>
