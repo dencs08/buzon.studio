2d3826c619ced6a3e3eb8cf92a4ef2aefc2f532b
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-01 19:08:56 +0100

    add: ProjectNext container

---
+++ vue/src/containers/Project/ProjectNext/ProjectNext.vue
@@ -0,0 +1,39 @@
+<template>
+    <section>
+        <div class="container mb-10">
+            <div>
+                <h5 class="float-right mb-10 font-color-dark">Zobacz nastepny projekt</h5>
+            </div>
+            <div class="relative">
+                <div class="link absolute top-[-10%]">
+                    <LinkPrimary class="uppercase font-color-primary font-family-header leading-none" to=""
+                        :text="$route.params.projectName" :split="true" :point="true" />
+                </div>
+                <div class="aspect-[3/1] ">
+                    <img src="/src/assets/images/portfolio/bielanypark/bielanypark8.jpg"
+                        class="object-cover w-full h-full" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import { LinkPrimary } from '../../../components'
+
+export default {
+    components: {
+        LinkPrimary
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h5 {
+    font-size: clamp(1rem, 1.5vw, 5rem);
+}
+
+.link {
+    font-size: clamp(1rem, 12.5vw, 20rem);
+}
+</style>
