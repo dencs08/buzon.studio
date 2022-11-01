f88e29f09b6344eae861f338640a8da1ea7aef2e
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-01 19:09:34 +0100

    add: new Project containers to Projekt view

--- vue/src/views/Projekt.vue
+++ vue/src/views/Projekt.vue
@@ -1,17 +1,40 @@
 <template>
     <main>
         <ProjectHero :name="$route.params.projectName" />
-        <ProjectInfo />
+        <ProjectInfo
+            info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum officiis modi, odit quod quisquam voluptatum, obcaecati."
+            :goals="['goal1', 'goal2', 'goal1', 'goal2', 'goal1', 'goal2']"
+            :responsibilities="['responsibilites1', 'responsibilites1', 'responsibilites1', 'responsibilites1']"
+            website="www.bielanypark.pl" />
+        <ProjectImageShowcase :name="$route.params.projectName" />
+        <ProjectMainDescription />
         <ProjectImages :name="$route.params.projectName" :imagesCount="1" />
-        <Footer />
+        <ProjectContactUs />
+        <ProjectNext />
     </main>
 </template>
 
 <script>
-import { Footer, ProjectHero, ProjectInfo, ProjectImages } from "../containers";
+import {
+    ProjectHero,
+    ProjectInfo,
+    ProjectImages,
+    ProjectImageShowcase,
+    ProjectMainDescription,
+    ProjectContactUs,
+    ProjectNext,
+} from "../containers";
 export default {
     name: "Projekt",
-    components: { Footer, ProjectHero, ProjectInfo, ProjectImages },
+    components: {
+        ProjectHero,
+        ProjectInfo,
+        ProjectImages,
+        ProjectImageShowcase,
+        ProjectMainDescription,
+        ProjectContactUs,
+        ProjectNext,
+    },
 };
 </script>
 
