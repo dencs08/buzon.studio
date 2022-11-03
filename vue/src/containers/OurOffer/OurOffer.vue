804f6c0b03720b9f007751802f32bc1d40545feb
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-03 21:06:54 +0100

    update: OurOffer container is using new OurOffer component

--- vue/src/containers/OurOffer/OurOffer.vue
+++ vue/src/containers/OurOffer/OurOffer.vue
@@ -1,21 +1,14 @@
 <template>
-    <div id="OurOffer">
-
-        <OurOfferColumn title="Produkty cyfrowe"
-            paragraph="Tworzymy i rozwijamy, strony internetowe, aplikacje, wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje potrzeby, tak abyś otrzymał najlepiej dostosowany produkt który pracuje dla Ciebie."
-            :text="['Strony internetowe', 'E-commerce', 'Aplikacje']"
-            :textSecond="['UI/UX', 'Projekty graficzne', 'Wizualizacje 3D']" :to="to" :bottomLinkText="bottomLinkText"
-            :buttons="buttons" />
-
-
-        <OurOfferColumn title="Branding"
-            paragraph="Dziś branding jest najważniejszym elementem każdego dobrze prosperującego biznesu, łatwe do zapamiętania logo, charakterystyczna identyfikacja wizualna czy dobrze obrana strategia marketingowa, ma ogromne znaczenie w jaki sposób będziesz odbierany na rynku"
-            :text="['Nazewnictwo', 'Identyfikacja wizualna', 'Logotypy']"
-            :textSecond="['Marketing', 'Socialmedia', 'Strategie biznesowe']" :to="to" :bottomLinkText="bottomLinkText"
-            :buttons="buttons" />
-
-    </div>
+    <section ref="section" id="OurOffer" class="bg-light" data-scroll data-scroll-speed="5">
+        <div class="container">
+            <OurOfferColumn title="Nasze możliwości"
+                :digital="['Strony internetowe', 'Projekty graficzne', 'Wizualizacje 3D', 'E-commerce', 'Aplikacje', 'UI/UX']"
+                :branding="['Identyfikacja wizualna', 'Nazewnictwo', 'Marketing online', 'Social media', 'Logotypy']"
+                :to="to" :bottomLinkText="bottomLinkText" :buttons="buttons" />
+        </div>
+    </section>
 </template>
+
 <script>
 import { OurOfferColumn } from '../../components'
 export default {
@@ -27,9 +20,10 @@ export default {
 
     components: {
         OurOfferColumn,
-    }
+    },
 }
 </script>
+
 <style lang="scss">
 @import './ouroffer.scss';
 </style>
