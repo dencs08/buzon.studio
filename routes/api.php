7422a6b97c0c80b61c78ee67385214b45b532454
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-04 00:18:32 +0100

    add: api routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\ProjectController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/projects', [ProjectController::class, 'index']);
+
+Route::prefix('/project')->group( function () {
+        Route::post('/store', [ProjectController::class, 'store']);
+        // Route::put('/{id}', [ProjectController::class, 'update']);
+        // Route::post('/{id}', [ProjectController::class, 'destroy']);
+    }
+);
