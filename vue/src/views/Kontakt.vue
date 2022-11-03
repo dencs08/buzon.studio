9d6a11b972117421bdeb3de89137aa67033855b3
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-03 21:05:20 +0100

    update: Kontakt view is now usig LadningPage container

--- vue/src/views/Kontakt.vue
+++ vue/src/views/Kontakt.vue
@@ -1,23 +1,13 @@
 <template>
     <main>
-        <section id="landing-page">
-            <div class="container pt-32 section-mb">
-                <h1 class="leading-[100%]">Zacznijmy owocną współpracę!</h1>
-
-                <h2 class="mb-5">
-                    Powiedz nam czego potrzebujesz, a my zrealizujemy to od A do
-                    Z.
-                </h2>
-
-                <!-- <x-arrow /> -->
-            </div>
-        </section>
+        <LandingPage h1="Zacznijmy owocną współpracę!"
+            h2="Powiedz nam czego potrzebujesz, a my zrealizujemy to od A do Z." />
         <EmailForm />
         <Footer />
     </main>
 </template>
 
 <script>
-import { Footer, EmailForm } from "../containers";
-export default { name: "Kontakt", components: { Footer, EmailForm } };
+import { Footer, EmailForm, LandingPage } from "../containers";
+export default { name: "Kontakt", components: { Footer, EmailForm, LandingPage } };
 </script>
