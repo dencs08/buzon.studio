31e0d857751efd448edb58f845c0358b175155e6
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-03 21:23:45 +0100

    add: LandingPage component

---
+++ vue/src/containers/LandingPage/LandingPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <section id="LandingPage">
+        <div class="container pt-[15vh] section-mb">
+            <h1 class="leading-[100%]">{{ h1 }}</h1>
+
+            <h2 class="mb-5">
+                {{ h2 }}
+            </h2>
+
+            <!-- <x-arrow /> -->
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        h1: String,
+        h2: String,
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
