98f175bb2b6907815d3962b35bde89dfd703b43f
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-03 21:23:06 +0100

    update: ProjectImages gap tweaks

--- vue/src/containers/Project/ProjectImageShowcase/ProjectImageShowcase.vue
+++ vue/src/containers/Project/ProjectImageShowcase/ProjectImageShowcase.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="ProjectImageShowcase" class="space-y-9 section-mb-s">
+    <section id="ProjectImageShowcase" class="space-y-2 sm:space-y-3 md:space-y-7 lg:space-y-9 section-mb-s">
         <ScrubTextSlider :images="['bielanypark3', 'bielanypark4', 'bielanypark5', 'bielanypark6', 'bielanypark7']" />
         <ScrubTextSlider :images="['bielanypark7', 'bielanypark6', 'bielanypark4', 'bielanypark3', 'bielanypark5']"
             :isReverse="true" />
--- vue/src/containers/Project/ProjectImages/ProjectImages.vue
+++ vue/src/containers/Project/ProjectImages/ProjectImages.vue
@@ -1,13 +1,13 @@
 <template>
     <section v-if="imagesCount" id="images" class="section-mb-s">
         <div class="container">
-            <div class="mb-16">
+            <div class="mb-4 md:mb-10 lg:mb-16">
                 <div class="overflow-hidden aspect-[22.5/10]">
                     <img :src="src()" class="object-cover min-w-full min-h-full h-full duration-500" alt="" />
                 </div>
             </div>
             <div class="grid grid-cols-2">
-                <div class="space-y-16 [&>*]:pr-8">
+                <div class="space-y-4 md:space-y-10 lg:space-y-16 [&>*]:pr-2 md:[&>*]:pr-5 lg:[&>*]:pr-8">
                     <div class="aspect-[2/1]">
                         <img src="/src/assets/images/portfolio/bielanypark/bielanypark8.jpg"
                             class="object-cover w-full h-full" alt="">
@@ -18,7 +18,7 @@
                     </div>
                 </div>
                 <div>
-                    <div class="pl-8 h-full">
+                    <div class="pl-2 md:pl-5 lg:pl-8 h-full">
                         <img :src="src()" class="object-cover w-full h-full" alt="">
                     </div>
                 </div>
