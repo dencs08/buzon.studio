d5778ab443080e540adb4d8594eac7f5943a19cc
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-03 21:22:46 +0100

    update: ProjectInfo layout improve & responsivness

--- vue/src/containers/Project/ProjectInfo/ProjectInfo.vue
+++ vue/src/containers/Project/ProjectInfo/ProjectInfo.vue
@@ -2,32 +2,51 @@
     <section id="info" class="section-mb-s">
         <div class="container">
 
-            <div class="lg:w-4/6 my-20">
+            <div class="lg:w-4/6 my-8 md:my-20">
                 <h4 class="font-family-primary font-color-primary">
                     {{ info }}
                 </h4>
             </div>
 
-            <div class="md:grid grid-cols-12 mb-5">
+            <div class="md:grid grid-cols-2 mb-5">
 
-                <div class="md:pr-10 col-span-6">
-                    <img src="/src/assets/images/portfolio/bielanypark/bielanypark2.jpg" alt="">
-                </div>
-
-                <div v-if="responsibilities" class="col-span-4">
-                    <div>
-                        <div>
-                            <h5 class="font-family-header font-color-primary my-0 uppercase">
-                                Co zrobiliśmy
-                            </h5>
-                        </div>
-                        <ul v-for="item in responsibilities" class="dots font-color-dark">
-                            <li class="mt-2">{{ item }}</li>
-                        </ul>
+                <div class="col-span-1 mb-6 md:mb-0">
+                    <div class="md:pr-10 col-span-6">
+                        <img src="/src/assets/images/portfolio/bielanypark/bielanypark2.jpg" alt="">
                     </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="block xs:grid grid-cols-2">
 
+                        <div v-if="responsibilities">
+                            <div class="mb-5 xs:mb-0">
+                                <div>
+                                    <h5 class="font-family-header font-color-primary my-0 uppercase">
+                                        Co zrobiliśmy
+                                    </h5>
+                                </div>
+                                <ul class="dots font-color-dark">
+                                    <li v-for="item in responsibilities" class="mt-2">{{ item }}</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div v-if="goals">
+                            <div>
+                                <div>
+                                    <h5 class="font-family-header font-color-primary my-0 uppercase">
+                                        Cel
+                                    </h5>
+                                </div>
+                                <ul class="dots font-color-dark">
+                                    <li v-for="item in goals" class="mt-2">{{ item }}</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
                     <div v-if="website">
-                        <div class="md:mt-16 lg:mt-40">
+                        <div class="mt-10 md:mt-16 lg:mt-40">
                             <span class="uppercase website font-color-dark">Sprawdź stronę
                                 <LinkPrimary :href="website" :primary="true" :text="website" target="_blank"
                                     rel="noopener" />
@@ -35,20 +54,6 @@
                         </div>
                     </div>
                 </div>
-
-                <div v-if="goals" class="col-span-2">
-                    <div>
-                        <div>
-                            <h5 class="font-family-header font-color-primary my-0 uppercase">
-                                Cel
-                            </h5>
-                        </div>
-                        <ul v-for="item in goals" class="dots font-color-dark">
-                            <li class="mt-2">{{ item }}</li>
-                        </ul>
-                    </div>
-                </div>
-
             </div>
         </div>
     </section>
