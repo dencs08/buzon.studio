8f03377c3e605aee5a0a12c31c7ffab2b8ed955c
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-05 00:41:02 +0100

    update: portfolioGrid container uses new /portfolio/name route

--- vue/src/containers/PortfolioGrid/PortfolioGrid.vue
+++ vue/src/containers/PortfolioGrid/PortfolioGrid.vue
@@ -1,11 +1,14 @@
 <template>
-    <div ref="wrapper" class="lg:h-[60vh] relative mb-[12.5vw]">
+    <div ref="wrapper" class="lg:h-[60vh] relative mb-[11vw]">
         <div ref="grid" class="lg:absolute lg:grid grid-cols-3">
-            <PortfolioColumn v-for="item in this.items" :items="item" />
+            <PortfolioColumn v-for="project in this.projects" :projects="project" />
         </div>
     </div>
 </template>
+
 <script>
+import axiosClient from '../../js/axios'
+
 import { PortfolioColumn, PortfolioItem } from '../../components'
 
 import gsap from 'gsap';
@@ -18,52 +21,57 @@ export default {
     },
 
     data() {
-        let items
-
-        return { items }
+        return { projects: [] }
     },
 
     mounted() {
-        this.items = [
-            [
-                { title: 'bielanypark', info: 'asd', categories: ['bielany1', 'bielany2'] },
-                { title: 'castle3d', info: 'asd', categories: ['bielany1', 'bielany2'] },
-                { title: 'jaguar', info: 'asd', categories: ['bielany1', 'bielany2'] }
-            ],
+        this.getProjects();
+    },
 
-            [
-                { title: 'zielonewidoki', info: 'asd', categories: ['bielany1', 'bielany2'], long: true },
-                { title: 'watch3d', info: 'asd', categories: ['bielany1', 'bielany2'], long: true },
-                { title: 'danfit', info: 'asd', categories: ['bielany1', 'bielany2'], long: true },
-                { title: 'ats', info: 'asd', categories: ['bielany1', 'bielany2'], long: true }
-            ],
-            [
-                { title: 'komb', info: 'asd', categories: ['bielany1', 'bielany2'] },
-                { title: 'meble', info: 'asd', categories: ['bielany1', 'bielany2'] },
-                { title: 'moss', info: 'asd', categories: ['bielany1', 'bielany2'] }
-            ]
-        ]
+    methods: {
+        async getProjects() {
+            const [first, second, third] = await Promise.all([
+                axiosClient.get('/api/projects/best'),
+                axiosClient.get('/api/projects/best'),
+                axiosClient.get('/api/projects/best'),
+            ]);
 
-        if (!window.matchMedia("(min-width: 1024px)").matches) return;
-        setTimeout(() => {
-            gsap.registerPlugin(ScrollTrigger)
-            let tlScroll;
-            tlScroll = gsap.timeline({ defaults: { ease: "none" }, paused: true });
-            tlScroll
-                .to(this.$refs.grid.children[0], { y: "-50%", duration: 3 })
-                .to(this.$refs.grid.children[1], { y: "-79%", duration: 3 }, "<")
-                .to(this.$refs.grid.children[2], { y: "-50%", duration: 3 }, "<")
 
-            ScrollTrigger.create({
-                trigger: this.$refs.wrapper,
-                start: "180% 100%",
-                end: "+=1500",
-                scroller: "#smoothScroll",
-                animation: tlScroll,
-                scrub: true,
-                pin: true,
-            })
-        }, 100);
+            this.projects.push(first.data.slice(0, 3))
+            this.projects.push(second.data.slice(3, 7))
+            this.projects.push(third.data.slice(7, 10))
+
+            this.projects[1][0].long = true
+            this.projects[1][1].long = true
+            this.projects[1][2].long = true
+            this.projects[1][3].long = true
+
+            this.scrollTrigger();
+        },
+
+        scrollTrigger() {
+            if (!window.matchMedia("(min-width: 1024px)").matches) return;
+            setTimeout(() => {
+                gsap.registerPlugin(ScrollTrigger)
+                let tlScroll;
+                tlScroll = gsap.timeline({ defaults: { ease: "none" }, paused: true });
+                tlScroll
+                    .to(this.$refs.grid.children[0], { y: "-50%", duration: 3 })
+                    .to(this.$refs.grid.children[1], { y: "-81%", duration: 3 }, "<")
+                    .to(this.$refs.grid.children[2], { y: "-50%", duration: 3 }, "<")
+
+                ScrollTrigger.create({
+                    trigger: this.$refs.wrapper,
+                    start: "180% 100%",
+                    end: "+=1500",
+                    scroller: "#smoothScroll",
+                    animation: tlScroll,
+                    scrub: true,
+                    pin: true,
+                })
+            }, 100);
+        }
+
     }
 }
 </script>
