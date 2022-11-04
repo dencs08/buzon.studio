817e0616a790d7afe195ebf77c685d8a5de627de
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-05 00:42:16 +0100

    update: OurWorksSlider works with api backend & scrollTrigger

--- vue/src/components/OurWorksSlider/OurWorksSlider.vue
+++ vue/src/components/OurWorksSlider/OurWorksSlider.vue
@@ -1,34 +1,63 @@
 <template>
     <div class="bg-transparent font-color-primary p-0">
-        <div class="whitespace-nowrap bg-transparent">
-            <SliderItem title="danfit"
-                info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam omnis magnam delectus aliquid, officiis quibusdam."
-                :category="['test1', 'test2', 'test3']" />
-            <SliderItem title="bielanypark"
-                info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam omnis magnam delectus aliquid, officiis quibusdam"
-                :category="['test14', 'test24', 'test34']" />
-            <SliderItem title="zielonewidoki"
-                info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam omnis magnam delectus aliquid, officiis quibusdam"
-                :category="['test13', 'test23', 'test33']" />
-            <SliderItem title="watch3d"
-                info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam omnis magnam delectus aliquid, officiis quibusdam"
-                :category="['test13', 'test23', 'test33']" />
-            <SliderItem title="komb"
-                info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam omnis magnam delectus aliquid, officiis quibusdam"
-                :category="['komb', 'test23', 'test33']" />
-            <SliderItem title="meble"
-                info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam omnis magnam delectus aliquid, officiis quibusdam"
-                :category="['test13', 'test23', 'test33']" />
+        <div ref="ourWorksSlider" class="whitespace-nowrap bg-transparent">
+            <SliderItem v-for="project in this.projects" :title="project.name" :info="project.descShort"
+                :category="project.categories" />
         </div>
     </div>
 </template>
 
 <script>
+import axiosClient from '../../js/axios'
+
 import { SliderItem } from '../'
+
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
 export default {
     components: {
         SliderItem
     },
+
+    data() {
+        return { projects: [] }
+    },
+
+    mounted() {
+        this.getProjects()
+    },
+
+    methods: {
+        getProjects() {
+            axiosClient.get('/api/projects/best')
+                .then(response => {
+                    response.data.length = 5;
+                    this.projects = response.data
+                    this.scrollTrigger()
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+
+        scrollTrigger() {
+            if (!window.matchMedia("(min-width: 1024px)").matches) return;
+
+            let tlScroll = gsap.timeline({ defaults: { ease: "none" }, paused: true })
+                .fromTo(this.$refs.ourWorksSlider, { x: '125%' }, { x: "-70%", duration: 8 })
+
+            gsap.registerPlugin(ScrollTrigger)
+            ScrollTrigger.create({
+                trigger: "#portfolio",
+                start: "50% 50%",
+                end: "+=5000",
+                scroller: "#smoothScroll",
+                animation: tlScroll,
+                scrub: true,
+            })
+        }
+    }
 }
 </script>
 
