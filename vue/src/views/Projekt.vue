17d0a67d7d75c4ff47e3d671f0d4f7406bc9234b
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-05 00:39:52 +0100

    update: Projekt.vue full functionality with backend

--- vue/src/views/Projekt.vue
+++ vue/src/views/Projekt.vue
@@ -1,20 +1,21 @@
 <template>
     <main>
         <ProjectHero :name="$route.params.projectName" />
-        <ProjectInfo
-            info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum officiis modi, odit quod quisquam voluptatum, obcaecati."
-            :goals="['goal1', 'goal2', 'goal1', 'goal2', 'goal1', 'goal2']"
-            :responsibilities="['responsibilites1', 'responsibilites1', 'responsibilites1', 'responsibilites1']"
-            website="https://bielanypark.pl/" />
-        <ProjectImageShowcase :imageSlider1="[]" :imageSlider2="[]" />
-        <ProjectMainDescription text="" image="" />
-        <ProjectImages :name="$route.params.projectName" :images="['test', 'test', 'test', 'test']" />
+        <ProjectInfo :info="this.project.descShort" :goals="this.project.goals"
+            :responsibilities="this.project.responsibilities" :website="this.project.website" :image="this.imageInfo" />
+        <ProjectImageShowcase :imageSlider1="this.imagesMain" :imageSlider2="this.imagesMain" />
+        <ProjectMainDescription :text="this.project.descLong" :image="this.imageMainDesc" />
+        <ProjectImages :name="$route.params.projectName" :images="this.imagesMain" />
         <ProjectContactUs />
-        <ProjectNext nextName="" nextImage="" />
+        <a :href="nextProjectUrl">
+            <ProjectNext :nextName="nextProject.name" :nextImage="nextProjectImg" />
+        </a>
     </main>
 </template>
 
 <script>
+import axiosClient from '../js/axios'
+
 import {
     ProjectHero,
     ProjectInfo,
@@ -35,6 +36,42 @@ export default {
         ProjectContactUs,
         ProjectNext,
     },
+
+    data() {
+        return { project: [], imageInfo: '', imagesSlider1: [], imagesSlider2: [], imageMainDesc: '', imagesMain: [], nextProject: [], nextProjectImg: '', nextProjectUrl: '' }
+    },
+
+    mounted() {
+        this.getProject()
+    },
+
+    methods: {
+        async getProject() {
+            const [current, next] = await Promise.all([
+                axiosClient.get('/api/project/' + this.$route.params.projectName),
+                axiosClient.get('/api/project/next/' + this.$route.params.projectName),
+            ]);
+
+            this.project = current.data
+
+            this.imageInfo = this.src(this.project.name, this.project.images[0]);
+            this.imageMainDesc = this.src(this.project.name, this.project.images[1]);
+            this.imagesSlider1 = this.project.images;
+
+            for (let i = 0; i < this.project.images.length; i++) {
+                const element = this.project.images[i];
+                this.imagesMain.push(this.src(this.project.name, element));
+            }
+
+            this.nextProject = next.data;
+            this.nextProjectImg = this.src(this.nextProject.name, this.nextProject.name + '_portfolio');
+            this.nextProjectUrl = `../projekt/${this.nextProject.name}`;
+        },
+
+        src(project, imageName) {
+            return `/src/assets/images/portfolio/${project}/${imageName}.jpg`;
+        },
+    }
 };
 </script>
 
