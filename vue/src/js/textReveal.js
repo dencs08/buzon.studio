fd78cb8257af9aea849581732c3a976620833795
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-06 21:06:09 +0100

    update: textReveal.js improve

--- vue/src/js/textReveal.js
+++ vue/src/js/textReveal.js
@@ -3,22 +3,46 @@ import { splitToLines } from "./cloneSplit"
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 
-export function scrubTextReveal(text, trigger) {
-    splitToLines(text);
-    let words = text.querySelectorAll(".line");
+export function textReveal(text, trigger, revealOnly, scrub, pin) {
+    if (!window.matchMedia("(min-width: 1024px)").matches) return;
 
-    gsap.registerPlugin(ScrollTrigger)
+    setTimeout(() => {
+        splitToLines(text);
+        let start, end;
+        let lines = text.querySelectorAll(".line");
 
-    let tlScroll = gsap.timeline({ defaults: { ease: "expo" }, paused: true });
-    tlScroll
-        .fromTo(words, { y: "100%" }, { y: "0%", duration: 0.5, stagger: 0.05 })
+        gsap.registerPlugin(ScrollTrigger)
 
-    ScrollTrigger.create({
-        trigger: trigger,
-        start: "20% 100%",
-        end: "+=1000",
-        scroller: "#smoothScroll",
-        animation: tlScroll,
-        scrub: 1,
-    })
+        let tlScroll = gsap.timeline({ defaults: { ease: "expo" }, paused: true });
+        tlScroll
+            .fromTo(lines, { y: "100%" }, { y: "0%", duration: 1, stagger: 0.05 })
+            .to(lines, { y: "0", duration: 0.75, stagger: 0.05 })
+
+        if (!revealOnly) tlScroll.to(lines, { y: "-100%", duration: 1, stagger: 0.05 });
+
+        if (scrub > 0 || scrub != false) start = "50% 50%";
+        if (scrub == 0 || scrub == false) start = "20% 100%";
+
+        end = 1000
+
+        ScrollTrigger.create({
+            trigger: trigger,
+            start: `${start}`,
+            end: `+=${end}`,
+            scroller: "#smoothScroll",
+            animation: tlScroll,
+            scrub: scrub,
+            pin: pin
+        })
+    }, 100);
 }
+
+// var wideScreen = window.matchMedia("(min-width: 800px)");
+// var narrowScreen = window.matchMedia("(max-width: 799px)");
+
+// if (wideScreen.matches) {
+
+// }
+// else {
+
+// }
