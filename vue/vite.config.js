55a2730812b0e6aab2e2ae6bd10806452133e087
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-06 18:14:48 +0100

    remove: laravel vite plugin

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import laravel from 'laravel-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    hmr: {
-      host: 'localhost',
-    },
-  },
   plugins: [
     vue(),
   ]
