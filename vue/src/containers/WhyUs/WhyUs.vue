3f9036380fafb392dade33dc9c5dea8c5778e8c2
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-06 21:06:33 +0100

    remove: redundant html code in WhyUs

--- vue/src/containers/WhyUs/WhyUs.vue
+++ vue/src/containers/WhyUs/WhyUs.vue
@@ -4,26 +4,6 @@
             <div class="mb-8 md:mb-12">
                 <SectionHeader v-if="header" mainHeader="Działamy po to abyś się rozwijał" />
             </div>
-            <!-- <Column title="Produkty cyfrowe"
-                paragraph="Tworzymy i rozwijamy, strony internetowe, aplikacje, wizualizacje, grafiki, projekty UI i UX adekwatnie pod Twoje potrzeby, tak abyś otrzymał najlepiej dostosowany produkt który pracuje dla Ciebie."
-                :ul="[
-                    'Strony internetowe',
-                    'E-commerce',
-                    'UI / UX',
-                    'Projekty graficzne',
-                    'Wizualizacje 3D',
-                    'Modelowanie 3D',
-                    'Aplikacje',
-                ]" :buttons="buttons" />
-            <Column title="Branding"
-                paragraph="Dziś branding jest najważniejszym elementem każdego dobrze prosperującego biznesu, łatwe do zapamiętania logo, charakterystyczna identyfikacja wizualna czy dobrze obrana strategia marketingowa, ma ogromne znaczenie w jaki sposób będziesz odbierany na rynku"
-                :ul="[
-                    'Nazewnictwo',
-                    'Identyfikacja wizualna',
-                    'Logotypy',
-                    'Marketing',
-                    'Strategie biznesowe w social mediach',
-                ]" :reverse="true" :buttons="buttons" /> -->
         </div>
 
         <div v-if="contactParagraph" class="container contact">
