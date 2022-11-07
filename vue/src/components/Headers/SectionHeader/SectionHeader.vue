ce6f53e321211c5f150fce7fc4cb9be1b30ffefe
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-08 00:53:30 +0100

    add: SectionHeader textReveal anim

--- vue/src/components/Headers/SectionHeader/SectionHeader.vue
+++ vue/src/components/Headers/SectionHeader/SectionHeader.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
-        <h4 class="sectionHeaderline" :class="{ headerline: noAccent === false }">{{ headerLine }}</h4>
-        <h3 class="sectionHeader" :class="{ big: big, break: wordBreak }">{{ mainHeader }}</h3>
+    <div ref="headerDiv">
+        <h4 class="sectionHeaderline" :class="{ headerline: noAccent === false }"><span ref="headerLine">{{ headerLine
+        }}</span></h4>
+        <h3 class="sectionHeader" :class="{ big: big, break: wordBreak }"><span ref="header">{{ mainHeader }}</span>
+        </h3>
     </div>
 </template>
 
 <script>
+import { textReveal } from '../../../js/textReveal'
+
 export default {
     props: {
         headerLine: String,
         mainHeader: String,
+        noReveal: Boolean,
         noAccent: {
             type: Boolean,
             default: () => false,
@@ -25,6 +30,11 @@ export default {
             default: () => false,
         },
     },
+
+    mounted() {
+        if (!this.noReveal) textReveal(this.$refs.headerLine, this.$refs.headerDiv, true, false, false, false);
+        if (!this.noReveal) textReveal(this.$refs.header, this.$refs.headerDiv, true, false, false, false);
+    }
 };
 </script>
 
