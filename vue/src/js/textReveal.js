2f73c4ccd72e984a2d08fa7cd53aa70b0839122c
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-07 16:11:55 +0100

    update: improve textReveal.js

--- vue/src/js/textReveal.js
+++ vue/src/js/textReveal.js
@@ -3,28 +3,28 @@ import { splitToLines } from "./cloneSplit"
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 
-export function textReveal(text, trigger, revealOnly, scrub, pin) {
-    if (!window.matchMedia("(min-width: 1024px)").matches) return;
-
+export function textReveal(text, trigger, revealOnly, scrub, pin, animateOpacity, startOffset) {
+    // if (!window.matchMedia("(min-width: 1024px)").matches) return;
+    if (!text) return;
     setTimeout(() => {
+        if (!startOffset) startOffset = 0;
         splitToLines(text);
-        let start, end;
+        let start, end, ease;
         let lines = text.querySelectorAll(".line");
+        let offset = Math.floor((offsetHeight(text, trigger) / trigger.offsetHeight) * 100) + (startOffset) + "%";
 
-        gsap.registerPlugin(ScrollTrigger)
+        if (scrub > 0 || scrub != false) start = "50% 50%", ease = "none";
+        if (scrub == 0 || scrub == false) start = `${offset} 80%`, ease = "expo";
 
-        let tlScroll = gsap.timeline({ defaults: { ease: "expo" }, paused: true });
+        let tlScroll = gsap.timeline({ defaults: { ease: ease }, paused: true });
         tlScroll
-            .fromTo(lines, { y: "100%" }, { y: "0%", duration: 1, stagger: 0.05 })
-            .to(lines, { y: "0", duration: 0.75, stagger: 0.05 })
+            .fromTo(lines, { y: "100%" }, { y: "0%", duration: 1.35, stagger: 0.05 })
 
+        if (animateOpacity) tlScroll.fromTo(lines, { opacity: 0 }, { opacity: 1, duration: 1, stagger: 0.05 }, "<");
         if (!revealOnly) tlScroll.to(lines, { y: "-100%", duration: 1, stagger: 0.05 });
-
-        if (scrub > 0 || scrub != false) start = "50% 50%";
-        if (scrub == 0 || scrub == false) start = "20% 100%";
-
         end = 1000
 
+        gsap.registerPlugin(ScrollTrigger)
         ScrollTrigger.create({
             trigger: trigger,
             start: `${start}`,
@@ -32,17 +32,13 @@ export function textReveal(text, trigger, revealOnly, scrub, pin) {
             scroller: "#smoothScroll",
             animation: tlScroll,
             scrub: scrub,
-            pin: pin
+            pin: pin,
+            // markers: true,
         })
     }, 100);
 }
 
-// var wideScreen = window.matchMedia("(min-width: 800px)");
-// var narrowScreen = window.matchMedia("(max-width: 799px)");
-
-// if (wideScreen.matches) {
-
-// }
-// else {
-
-// }
+function offsetHeight(el, div) {
+    let offset = el.offsetTop - div.offsetTop;
+    return offset;
+}
