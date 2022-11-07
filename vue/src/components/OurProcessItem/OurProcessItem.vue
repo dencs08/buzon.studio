b6c751e1fc9237e476a57f59d6b5a35118522b5f
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-08 00:56:27 +0100

    add: OurProcessItem paragraph textReveal

--- vue/src/components/OurProcessItem/OurProcessItem.vue
+++ vue/src/components/OurProcessItem/OurProcessItem.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="link-activate-wrapper" @mouseover="mouseOver" @mouseleave="mouseLeave">
-        <ItemHeader :text="title" :small="true" :move="true" headerClass="font-color-secondary mb-2" ref="itemHeader" />
+    <div ref="trigger" class="link-activate-wrapper" @mouseover="mouseOver" @mouseleave="mouseLeave">
+        <ItemHeader ref="itemHeader" :text="title" :small="true" :move="true" headerClass="font-color-secondary mb-2" />
 
         <p class="font-color-dark pr-0 md:pr-20">
-            {{ paragraph }}
+            <span ref="paragraph">{{ paragraph }}</span>
         </p>
     </div>
 </template>
 
 <script>
 import { ItemHeader } from "../";
+import { textRevealInline } from '../../js/textReveal'
 
 export default {
     data() {
@@ -24,6 +25,10 @@ export default {
     },
     components: {
         ItemHeader,
+    },
+
+    mounted() {
+        textRevealInline(this.$refs.paragraph, this.$refs.trigger, true, false, false, false);
     },
 
     methods: {
