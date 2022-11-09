e24c8bc57cdf40f84eab34684e1d89ee6b51b928
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-09 14:26:44 +0100

    remove: redundant code EmailController

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -11,12 +11,6 @@ class EmailController extends Controller
 {
     public function emailSend(Request $request)
     {
-      // dump($request->name);
-      // dump($request->email);
-      // dump($request->content);
-      // dump($request->controlInfo);
-      // dd($request);
-
         $request->validate([
             'email' => 'required',
             'name' => 'required',
@@ -29,12 +23,11 @@ class EmailController extends Controller
           'email' => $request->email,
           'content' => $request->content,
           'controlInfo' => $request->controlInfo,
-          // 'logo' => asset('/images/logos/buzonstudio_black.svg')
         ];
 
         Mail::to($request->email)->send(new EmailThanksForContact($data));
         Mail::to(env('MAIL_USERNAME', 'hej@buzon.studio'))->send(new EmailFormUserSent($data));
 
-          return;
+        return;
     }
 }
