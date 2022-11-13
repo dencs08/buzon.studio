8b0eb5b6a7cb6aa070b19b8f85fb69365982b40c
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-13 21:07:05 +0100

    update: AboutUs add .reveal class & dedicated animation for mobiles

--- vue/src/containers/AboutUs/AboutUs.vue
+++ vue/src/containers/AboutUs/AboutUs.vue
@@ -1,7 +1,7 @@
 <template>
     <section ref="section" id="aboutUs">
         <div class="container w-11/12 grid content-center text-center w-screen h-[30vh] lg:h-screen">
-            <p class="font-color-dark" ref="text">
+            <p class="font-color-dark reveal" ref="text">
                 <b class="uppercase font-bold">{{ name }}</b> to 5 lat
                 doświadczenia w tworzeniu projektów i pomaganiu markom w
                 optymalizacji ich działań marketingowych.
@@ -31,11 +31,20 @@ export default {
         let text = this.$refs.text;
         let section = this.$refs.section;
 
-        textRevealInline(text, section, false, 1, true, false);
+        if (window.matchMedia("(min-width: 1024px)").matches) textRevealInline(text, section, false, 1, true, false);
+
+
+        if (!window.matchMedia("(max-width: 1024px)").matches) return;
+        textRevealInline(text, section, true, false, false, false);
+
     },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "./aboutus.scss";
+
+.reveal {
+    visibility: hidden;
+}
 </style>
