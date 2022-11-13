4bf323648e442ace69622721ac84b42121692424
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-13 21:02:25 +0100

    fix: Portfolio dummy load grid is now responsive

--- vue/src/views/Portfolio.vue
+++ vue/src/views/Portfolio.vue
@@ -5,22 +5,24 @@
                 <PortfolioGrid />
             </template>
             <template #fallback>
-                <div class="container">
-                    <div class="grid grid-cols-3 space-x-10">
-                        <div class="h-screen space-y-10">
-                            <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
-                            <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
-                        </div>
-                        <div class="h-screen space-y-10">
-                            <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
-                            <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
-                        </div>
-                        <div class="h-screen space-y-10">
-                            <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
-                            <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                <transition name="fade" mode="out-in">
+                    <div class="container">
+                        <div class="md:grid grid-cols-3 space-x-10">
+                            <div class="h-screen space-y-10">
+                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                            </div>
+                            <div class="h-screen space-y-10">
+                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                            </div>
+                            <div class="h-screen space-y-10">
+                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </transition>
             </template>
         </Suspense>
 
@@ -39,3 +41,15 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.65s ease-in-out;
+}
+</style>
