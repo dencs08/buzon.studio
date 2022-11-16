7dc11b24a67efc9b7e94031db123d44d53be24a6
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-16 14:09:33 +0100

    update: ButtonTral scss to tailwind + reveal animation

--- vue/src/components/CallToAction/Buttons/ButtonTrail/ButtonTrail.vue
+++ vue/src/components/CallToAction/Buttons/ButtonTrail/ButtonTrail.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="button-trail">
-        <form>
-            <router-link to="Kontakt">
+    <div ref="btnWrapper" class="button-trail">
+        <form ref="btn" class="reveal">
+            <router-link to="Kontakt"
+                class="relative inline-block w-[75%] py-5 md:py-6 md:w-auto md:px-20 uppercase overflow-hidden duration-500 text-highlight">
                 <span></span>
                 <span></span>
                 <span></span>
@@ -12,12 +13,22 @@
     </div>
 </template>
 <script>
+import { elementReveal } from '../../../../js/textReveal'
+
 export default {
     props: {
         text: String,
     },
+
+    mounted() {
+        elementReveal(this.$refs.btn, this.$refs.btnWrapper);
+    }
 };
 </script>
 <style lang="scss" scoped>
 @import "./buttontrail.scss";
+
+.reveal {
+    visibility: hidden;
+}
 </style>
