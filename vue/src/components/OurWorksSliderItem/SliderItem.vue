f02750f7de5d94eb7db15306412f0f357789df95
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-16 14:11:58 +0100

    add: SliderItem reveal animation

--- vue/src/components/OurWorksSliderItem/SliderItem.vue
+++ vue/src/components/OurWorksSliderItem/SliderItem.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="relative mb-6 lg:mb-0 lg:ml-5 lg:mr-20 lg:inline-block lg:w-[50vw] xl:w-[45vw]">
-        <ImageShowcase :title="title" :info="info" :category="category" />
+    <div ref="imageWrapper" class="relative mb-6 lg:mb-0 lg:ml-5 lg:mr-20 lg:inline-block lg:w-[50vw] xl:w-[45vw]">
+        <ImageShowcase ref="imageShowcase" :title="title" :info="info" :category="category" class="reveal" />
     </div>
 </template>
 
 <script>
 import { ItemHeader, ImageShowcase } from "../";
+import { elementReveal } from "../../js/textReveal";
 
 export default {
     components: {
@@ -17,9 +18,22 @@ export default {
         info: String,
         category: Array,
     },
+
+    mounted() {
+        if (!window.matchMedia("(max-width: 1024px)").matches) return;
+        setTimeout(() => {
+            elementReveal(this.$refs.imageShowcase.$el, this.$refs.imageWrapper);
+        }, 500);
+    }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "./slideritem.scss";
+
+@media screen and (max-width: 1024px) {
+    .reveal {
+        visibility: hidden;
+    }
+}
 </style>
