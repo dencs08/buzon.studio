52d874619b19ed474038e1ae492bf4a44f1d78ea
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-23 00:46:21 +0100

    update: Portfolio loading grid color & blinking

--- vue/src/views/Portfolio.vue
+++ vue/src/views/Portfolio.vue
@@ -7,18 +7,18 @@
             <template #fallback>
                 <transition name="fade" mode="out-in">
                     <div class="container">
-                        <div class="md:grid grid-cols-3 space-x-10">
+                        <div class="md:grid grid-cols-3 space-x-10 loadingGrid">
                             <div class="h-screen space-y-10">
-                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
-                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                                <div class="bg-zinc-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                                <div class="bg-zinc-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
                             </div>
                             <div class="h-screen space-y-10">
-                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
-                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                                <div class="bg-zinc-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                                <div class="bg-zinc-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
                             </div>
                             <div class="h-screen space-y-10">
-                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
-                                <div class="bg-slate-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                                <div class="bg-zinc-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
+                                <div class="bg-zinc-900 min-h-[70vh] max-h-[80vh] w-full aspect-[7/10]"></div>
                             </div>
                         </div>
                     </div>
@@ -48,8 +48,30 @@ export default {
     opacity: 0;
 }
 
+.fade-enter-to {
+    opacity: 0.5;
+}
+
 .fade-enter-active,
 .fade-leave-active {
     transition: opacity 0.65s ease-in-out;
+}
+
+.loadingGrid {
+    animation: blink 1s 0.65s linear infinite;
+}
+
+@keyframes blink {
+    0% {
+        opacity: inherit;
+    }
+
+    50% {
+        opacity: 0.25;
+    }
+
+    100% {
+        opacity: inherit;
+    }
 }
 </style>
