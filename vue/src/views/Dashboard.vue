c02bb2294adee9b74cbc90cb8ba9119dce4e7325
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-02 10:56:34 +0100

    add: Dashboard & Login components

---
+++ vue/src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    dashboard
+</template>
+<script>
+import { SecretComponent } from '../components'
+export default {
+    components: {
+        SecretComponent,
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
---
+++ vue/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <div class="h-screen grid place-content-center">
+            <SecretComponent />
+        </div>
+    </section>
+</template>
+<script>
+import { SecretComponent } from '../components'
+export default {
+    components: {
+        SecretComponent
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
