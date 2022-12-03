9f2742ca27d7616bdd8044c8401bdc9d8dacba11
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-03 19:24:04 +0100

    add: Dashboard layout

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,14 +1,39 @@
 <template>
-    dashboard
+    <div>
+        <div>
+            <NavbarAdmin />
+        </div>
+        <div class="flex">
+            <div>
+                <Sidebar class="h-screen" />
+            </div>
+            <div class="bg-lighter w-full container pt-4">
+                <router-view v-slot="{ Component }">
+                    <transition name="quickFade" mode="out-in">
+                        <component :is="Component" />
+                    </transition>
+                </router-view>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
-import { SecretComponent } from '../components'
+import { Sidebar, NavbarAdmin } from '../components'
 export default {
     components: {
-        SecretComponent,
+        Sidebar,
+        NavbarAdmin
     }
 }
 </script>
 <style lang="scss">
+.quickFade-enter-from,
+.quickFade-leave-to {
+    opacity: 0;
+}
 
+.quickFade-enter-active,
+.quickFade-leave-active {
+    transition: opacity 0.125s ease-out;
+}
 </style>
