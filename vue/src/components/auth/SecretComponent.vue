e8ac65af7f3fefe81a54255e63b879acd07da8cd
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-04 22:13:30 +0100

    update: display error login

--- vue/src/components/auth/SecretComponent.vue
+++ vue/src/components/auth/SecretComponent.vue
@@ -3,8 +3,8 @@
         <div class="container">
             <div class="p-10 bg-zinc-900 rounded-xl sm:min-w-[450px] w-[95vw] sm:w-full">
                 <h4 class="mb-8">Zaloguj sie.</h4>
-                <form class="space-y-8">
-                    <div class="form-field">
+                <form @submit.prevent="handleLogin">
+                    <div class="form-field mb-8">
                         <input id="name" name="name" v-model="formData.name" class="input-text js-input" type="text"
                             placeholder=" " required />
                         <label for="name">
@@ -12,7 +12,7 @@
                         </label>
                     </div>
 
-                    <div class="form-field">
+                    <div class="form-field mb-4">
                         <input id="password" name="password" v-model="formData.password" class="input-text js-input"
                             type="password" placeholder=" " required />
                         <label for="name">
@@ -20,9 +20,10 @@
                         </label>
                     </div>
 
+                    <p v-if="error" class="text-red-500 mb-4">{{ error }}</p>
+
                     <div>
-                        <Button text="Login" type="submit" @click="handleLogin" :primary="true" :small="true"
-                            :noReveal="true" />
+                        <Button text="Login" type="submit" :primary="true" :small="true" :noReveal="true" />
                     </div>
                 </form>
             </div>
@@ -40,7 +41,7 @@ export default {
     },
     data() {
         return {
-            secrets: [],
+            error: null,
             formData: {
                 name: '',
                 password: ''
@@ -53,16 +54,13 @@ export default {
 
     methods: {
         handleLogin(e) {
-            e.preventDefault();
-
             axiosAuthenticator.get('/sanctum/csrf-cookie').then(response => {
                 axiosAuthenticator.post('/api/login', this.formData)
                     .then(response => {
-                        console.log(response);
                         this.$router.push({ path: '/dashboard' })
                     })
                     .catch(error => {
-                        // console.error(error);
+                        this.error = 'Invalid name or password.';
                     })
             });
         },
