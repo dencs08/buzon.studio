931dedd6ed391165a9e74be2f0e3d3d1aeca46d9
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-06 11:47:33 +0100

    update: SecretComponent doesnt hold scss now

--- vue/src/components/auth/SecretComponent.vue
+++ vue/src/components/auth/SecretComponent.vue
@@ -53,7 +53,7 @@ export default {
     },
 
     methods: {
-        handleLogin(e) {
+        handleLogin() {
             axiosAuthenticator.get('/sanctum/csrf-cookie').then(response => {
                 axiosAuthenticator.post('/api/login', this.formData)
                     .then(response => {
@@ -69,88 +69,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "../../styles/_variables.scss";
 
-.form-field {
-    position: relative;
-
-    .input-text {
-        display: block;
-        width: 100%;
-
-        border: 1px solid #6b6b6b80 !important;
-        border-radius: 100px;
-
-        padding-top: clamp(1em, 1.1vw, 3em);
-        padding-bottom: clamp(1em, 1.1vw, 3em);
-        padding-left: clamp(1em, 1.1vw, 3em);
-        padding-right: clamp(1em, 1.1vw, 3em);
-
-        background-color: #ffffff0d !important;
-
-        color: $font-color-primary !important;
-
-        transition: 0.5s border;
-
-        &:focus {
-            outline: none;
-            border: solid 1px $primary-color !important;
-        }
-    }
-
-
-    label {
-        position: absolute;
-        left: clamp(2.25rem, 1.65vw, 4rem);
-        top: clamp(1rem, 1.1vw, 2.75rem);
-
-        font-size: clamp(0.9rem, 1vw, 1.35rem);
-        font-weight: 300;
-        color: rgb(126, 126, 126);
-
-        transition: all 0.25s ease-in-out;
-    }
-
-    input:focus~label,
-    input:valid~label {
-        top: -24px;
-        left: 10px;
-        color: $primary-color;
-        font-size: $font-size-base * 0.9;
-        font-weight: 500;
-
-        input {
-            border: $primary-color 1px solid !important;
-        }
-    }
-
-    input:not(:placeholder-shown):not(:focus):invalid {
-        border: 1px solid red !important;
-        color: red;
-    }
-
-    input:not(:placeholder-shown):not(:focus):invalid~label {
-        color: red;
-        font-size: 14px;
-        left: 10px;
-        top: -20px;
-    }
-}
-
-
-@media (max-width: $lg-screen-size) {
-    .form-field {
-        label {
-            top: 17px;
-        }
-    }
-}
-
-@media (max-width: $md-screen-size) {
-    .form-field {
-        label {
-            top: 19px;
-        }
-    }
-}
 </style>
