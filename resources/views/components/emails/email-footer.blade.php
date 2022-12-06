75f4af823975e3c9b13ecde5e1e291f093d1663c
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-06 11:46:14 +0100

    add: EmailFooter & EmailNavbar blade components

---
+++ resources/views/components/emails/email-footer.blade.php
@@ -0,0 +1,125 @@
+<tr>
+          <td
+            style="
+              padding: 0;
+              background-color: #1b1b1b;
+              padding: 45px 25px 45px 25px;
+            "
+          >
+            <table
+              role="presentation"
+              width="100%"
+              class="footer"
+              style="border-spacing: 0; color: #f8f3f1; text-align: center"
+            >
+              <tr>
+                <td style="padding: 0">
+                  <h3 style="font-size: 18px; font-weight: 600; margin: 0; color: #fcfcfc; font-family: 'Poppins', sans-serif;">
+                    Zostańmy w kontakcie!
+                  </h3>
+                  <ul class="social" style="margin: 25px 0 0 0; padding: 0">
+                    <li
+                      style="
+                        text-decoration: none;
+                        list-style-type: none;
+                        display: inline-block;
+                        margin: 0 5px 0 5px;
+                      "
+                    >
+                      <a
+                        href="https://www.facebook.com/buzonstudio"
+                        target="_blank"
+                        rel="noopener"
+                        style="
+                          color: rgb(124, 224, 93);
+                          text-decoration: none;
+                          color: #60de9f !important;
+                        "
+                        ><img
+                          width="40px"
+                          height="40px"
+                          src="https://www.dropbox.com/s/afulsoz8yp1z3h7/facebook.png?raw=1"
+                          alt="logo facebook"
+                          style="border: 0"
+                      /></a>
+                    </li>
+                    <li
+                      style="
+                        text-decoration: none;
+                        list-style-type: none;
+                        display: inline-block;
+                        margin: 0 5px 0 5px;
+                      "
+                    >
+                      <a
+                        href="https://www.buzon.studio/kontakt"
+                        target="_blank"
+                        rel="noopener"
+                        style="
+                          color: rgb(124, 224, 93);
+                          text-decoration: none;
+                          color: #60de9f !important;
+                        "
+                        ><img
+                          width="40px"
+                          height="40px"
+                          src="https://www.dropbox.com/s/228cyfigoryvrzc/email.png?raw=1"
+                          alt="logo email"
+                          style="border: 0"
+                      /></a>
+                    </li>
+                    <li
+                      style="
+                        text-decoration: none;
+                        list-style-type: none;
+                        display: inline-block;
+                        margin: 0 5px 0 5px;
+                      "
+                    >
+                      <a
+                        href="https://www.instagram.com/buzonstudio/"
+                        target="_blank"
+                        rel="noopener"
+                        style="
+                          color: rgb(124, 224, 93);
+                          text-decoration: none;
+                          color: #60de9f !important;
+                        "
+                        ><img
+                          width="40px"
+                          height="40px"
+                          src="https://www.dropbox.com/s/800nz3bqfvh58ge/ig.png?raw=1"
+                          alt="logo instagram"
+                          style="border: 0"
+                      /></a>
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <!-- BORDER -->
+        <tr align="center">
+          <td
+            height="50px"
+            style="
+              padding: 0;
+              font-family: 'Poppins', sans-serif;
+              font-size: 10px;
+              text-align: center;
+              background-color: #000000;
+            "
+            class="text-center font-family-primary font-size-tertiary"
+          >
+            <span
+              >© 2022
+              <a
+                href="https://www.buzon.studio"
+                style="color: #60de9f !important; font-family: 'Poppins', sans-serif;"
+                >buzon.studio</a
+              >
+              Wszelkie prawa zastrzeżone.
+            </span>
+          </td>
+        </tr>
---
+++ resources/views/components/emails/email-navbar.blade.php
@@ -0,0 +1,87 @@
+<tr>
+  <td
+    style="
+      font-family: 'Poppins', sans-serif;
+      background-color: #1b1b1b;
+      padding: 25px 20px 25px 20px;
+      height: 30px;
+    "
+  >
+    <table role="presentation" width="100%" style="border-spacing: 0">
+      <tr>
+        <td>
+          <a
+          href="https://www.buzon.studio/start"
+          style="
+            color: #ffffff;
+            text-decoration: none;
+            list-style-type: none;
+            margin-right: 7px;
+            margin-left: 7px;
+            color: #60de9f !important;
+          "
+          >
+            <img
+              src="https://www.dropbox.com/s/uzm74at6ook4r4r/buzonstudio_white_cropped.png?raw=1"
+              alt="logo buzon.studio"
+              width="65px"
+              style="border: 0; float: left; max-width: 100%;"
+            />
+          </a>
+        </td>
+        <td>
+          <ul style="margin: 0px; padding: 0px">
+            <li style="list-style-type: none; float: right">
+              <a
+                href="https://www.buzon.studio/start"
+                target="_blank"
+                rel="noopener"
+                style="
+                  color: #ffffff;
+                  text-decoration: none;
+                  list-style-type: none;
+                  margin-right: 7px;
+                  margin-left: 7px;
+                  color: #60de9f !important;
+                "
+                >Start</a
+              >
+            </li>
+            <li style="list-style-type: none; float: right">
+              <a
+                href="https://www.buzon.studio/oferta"
+                target="_blank"
+                rel="noopener"
+                style="
+                  color: #ffffff;
+                  text-decoration: none;
+                  list-style-type: none;
+                  margin-right: 7px;
+                  margin-left: 7px;
+                  color: #60de9f !important;
+                "
+                >Oferta</a
+              >
+            </li>
+            <li style="list-style-type: none; float: right">
+              <a
+                href="https://www.buzon.studio/portfolio"
+                target="_blank"
+                rel="noopener"
+                style="
+                  color: #ffffff;
+                  text-decoration: none;
+                  list-style-type: none;
+                  margin-right: 7px;
+                  margin-left: 7px;
+                  color: #60de9f !important;
+                "
+                >Portfolio</a
+              >
+            </li>
+          </ul>
+        </td>
+      </tr>
+    </table>
+  </td>
+</tr>
