f0e8471a809264e90346745e5080a25c9f66ca6a
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-06 11:45:37 +0100

    add: EmailPricing & response controller functions

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\EmailThanksForContact;
 use App\Mail\EmailFormUserSent;
+use App\Mail\EmailResponse;
+use App\Mail\EmailPricing;
 
 class EmailController extends Controller
 {
@@ -29,5 +31,34 @@ class EmailController extends Controller
         Mail::to(env('MAIL_USERNAME', 'hej@buzon.studio'))->send(new EmailFormUserSent($data));
 
         return;
+    }
+
+    public function emailResponse(Request $request){
+
+        $data = [
+          'name' => $request->name,
+          'email' => $request->email,
+          'content' => $request->content,
+        ];
+
+        Mail::to($request->email)->send(new EmailResponse($data));
+    }
+
+    public function emailPricing(Request $request){
+
+        $data = [
+          'h1' => $request->h1,
+          'h2' => $request->h2,
+          'main' => $request->main,
+          'bottomh3' => $request->bottomh3,
+          'bottomText' => $request->bottomText,
+          'table' => $request->table,
+          'totalPrice' => $request->totalPrice
+        ];
+
+        // return response()->json($request, 200);
+
+        Mail::to($request->to)->send(new EmailPricing($data));
+        // return view('emails/email-pricing-table')->with($data);
     }
 }
